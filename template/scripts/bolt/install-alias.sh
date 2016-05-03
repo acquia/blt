115a3464de51cf73b6a460a8ec64952a4152c883
Dane Powell <git@danepowell.com>
2016-05-03 08:58:12 -0500

    Make Bolt alias compatible with all Unix environments.

--- template/scripts/bolt/install-alias.sh
+++ template/scripts/bolt/install-alias.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-if [ -f ~/.bash_profile ]; then
-  if [ ! "`grep 'function bolt' ~/.bash_profile`" ]; then
-     DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-     cat $DIR/alias >> ~/.bash_profile
-     echo "Added alias for bolt to ~/.bash_profile."
-     echo "Restart your terminal session to use the new command."
+if [ -f ~/.bashrc ]; then
+  if [ ! "`grep 'function bolt' ~/.bashrc`" ]; then
+    # Check for aliases in old-style .bash_profile.
+    # This check can be removed after everyone has moved to .bashrc.
+    if [ -f ~/.bash_profile ]; then
+      if [ "`grep 'function bolt' ~/.bash_profile`" ]; then
+        echo "Alias for bolt already exists in ~/.bash_profile"
+        exit
+      fi
+    fi
+    DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    cat $DIR/alias >> ~/.bashrc
+    echo "Added alias for bolt to ~/.bashrc."
+    echo "Restart your terminal session to use the new command."
   else
-    echo "Alias for bolt already exists in ~/.bash_profile"
+    echo "Alias for bolt already exists in ~/.bashrc"
   fi
 else
-  echo "~/.bash_profile was not found. Could not install bolt alias."
+  echo "~/.bashrc was not found. Could not install bolt alias."
 fi
