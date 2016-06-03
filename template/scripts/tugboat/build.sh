df43cf3ad3b64d807e1702c14f91ab3fb6c9b156
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-06-03 19:13:32 -0400

    Fixing incorrect references to bolt.

--- template/scripts/tugboat/build.sh
+++ template/scripts/tugboat/build.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Update current database to reflect the state of the Drupal file system
-$DIR/../../bolt.sh ci:update
+$DIR/../../blt.sh ci:update
