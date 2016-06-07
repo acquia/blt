a660393563134cd1827331819ccab3fd84ba4286
Dane Powell <git@danepowell.com>
2016-06-07 14:18:20 -0500

    Catch PHP messages with Behat.

--- template/tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ template/tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -21,4 +21,38 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
 
   }
 
+  /**
+   * The BeforeSuite hook is run before any feature in the suite runs.
+   *
+   * @BeforeSuite
+   */
+  public static function prepare($event) {
+    if (db_table_exists('watchdog')) {
+      db_truncate('watchdog')->execute();
+    }
+  }
+
+  /**
+   * The AfterScenario hook is run after executing a scenario.
+   *
+   * @AfterScenario
+   */
+  public function afterScenario($event) {
+    if (db_table_exists('watchdog')) {
+      $log = db_select('watchdog', 'w')
+        ->fields('w')
+        ->condition('w.type', 'php', '=')
+        ->execute()
+        ->fetchAll();
+      if (!empty($log)) {
+        foreach ($log as $error) {
+          // Make the substitutions easier to read in the log.
+          $error->variables = unserialize($error->variables);
+          print_r($error);
+        }
+        throw new \Exception('PHP errors logged to watchdog in this scenario.');
+      }
+    }
+  }
+
 }
