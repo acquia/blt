335432c2884aa1aa1b20f32cc892829e94ea1742
David Pagini <davidpagini@gmail.com>
2016-06-08 10:49:58 -0400

    Adding Drupal VM 3.1 integration.

---
+++ template/scripts/drupal-vm/drupal-vm.aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+
+// [vagrant_machine_name].local
+$aliases['${project.machine_name}.local'] = array(
+  // /var/www/[vagrant_machine_name]/docroot
+  'root' => '/var/www/${project.machine_name}/docroot',
+  // vagrant_hostname
+  'uri' => '${project.local.hostname}',
+  // vagrant_hostname
+  'remote-host' => '${project.local.hostname}',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+);
