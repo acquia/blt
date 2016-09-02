5c07e0ab148c4f57f030f73477a3ea3b7c5b60dd
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-09-02 14:35:06 -0400

    Fixes #211: Store filepaths intelligently. (#359)

--- template/docroot/sites/default/settings/filesystem.settings.php
+++ template/docroot/sites/default/settings/filesystem.settings.php
@@ -7,10 +7,12 @@
 
 $settings['file_public_path'] = 'sites/default/files';
 
-/**
- * Acquia cloud file paths.
- */
-if ($is_ah_env) {
+// ACSF file paths.
+if ($is_acsf) {
+  $settings['file_private_path'] = "/mnt/gfs/$ah_group.$ah_env/files-private/$acsf_db_name";
+}
+// Acquia cloud file paths.
+elseif ($is_ah_env) {
   $config['system.file']['path']['temporary'] = '/mnt/tmp/' . $_ENV['AH_SITE_NAME'];
-  $settings['file_private_path'] = '/mnt/files/' . $_ENV['AH_SITE_NAME'] . '/files-private';
+  $settings['file_private_path'] = "/mnt/files/$ah_group.$ah_env/files-private";
 }
