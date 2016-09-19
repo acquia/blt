0d1bbe44b396288453f76c450c45ae8b08daf319
Lindsey Catlett <lindsey.catlett@gmail.com>
2016-09-19 11:00:22 -0400

    Bugfix for ACSF db name. (#418)

--- settings/blt.settings.php
+++ settings/blt.settings.php
@@ -83,7 +83,7 @@ $settings['hash_salt'] = file_get_contents(DRUPAL_ROOT . '/../salt.txt');
 /**
  * Acquia Cloud settings.
  */
-if ($is_ah_env && file_exists('/var/www/site-php')) {
+if ($is_ah_env && file_exists('/var/www/site-php' && !$is_acsf)) {
   require "/var/www/site-php/{$_ENV['AH_SITE_GROUP']}/{$_ENV['AH_SITE_GROUP']}-settings.inc";
 
   // Store API Keys and things outside of version control.
