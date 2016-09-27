8a743c6632b0dca0fe3c6283d0d92aec5e5494e4
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-09-27 14:20:45 -0400

    Improving Behat + Drupal VM integration. (#462)
    * Fixing permissions on post-provision.sh.
    
    * Fixing Behat + DrupalVM integration.

--- scripts/drupal-vm/post-provision.sh
+++ scripts/drupal-vm/post-provision.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-export $PATH=/var/www/$(hostname)/vendor/bin:$PATH
+# Append vendor/bin to $PATH via ~/.bashrc modification.
+grep -q -F 'export PATH=/var/www/$(hostname)/vendor/bin:$PATH' .bashrc || echo 'export PATH=/var/www/$(hostname)/vendor/bin:$PATH' >> .bashrc
