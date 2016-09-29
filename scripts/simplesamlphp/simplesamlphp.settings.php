54398ad90108cb68e72b9cd735083ab5d79c0020
dooleymatt <matt.dooley@acquia.com>
2016-09-29 13:15:54 -0400

    Updates path to library in simplesamlphp.settings.php.

--- scripts/simplesamlphp/simplesamlphp.settings.php
+++ scripts/simplesamlphp/simplesamlphp.settings.php
@@ -2,12 +2,12 @@
 
 // SimpleSAMLphp configuration
 # Provide universal absolute path to the installation.
-if (isset($_ENV['AH_SITE_NAME']) && is_dir('/var/www/html/' . $_ENV['AH_SITE_NAME'] . '/simplesamlphp')) {
-  $settings['simplesamlphp_dir'] = '/var/www/html/' . $_ENV['AH_SITE_NAME'] . '/simplesamlphp';
+if (isset($_ENV['AH_SITE_NAME']) && is_dir('/var/www/html/' . $_ENV['AH_SITE_NAME'] . '/vendor/simplesamlphp/simplesamlphp')) {
+  $settings['simplesamlphp_dir'] = '/var/www/html/' . $_ENV['AH_SITE_NAME'] . '/vendor/simplesamlphp/simplesamlphp';
 }
 else {
   // Local SAML path.
   if (is_dir(DRUPAL_ROOT . '/../simplesamlphp')) {
-    $settings['simplesamlphp_dir'] = DRUPAL_ROOT . '/../simplesamlphp';
+    $settings['simplesamlphp_dir'] = DRUPAL_ROOT . '/../vendor/simplesamlphp/simplesamlphp';
   }
 }
