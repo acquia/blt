d70eb6b52baf96ea5ddb0357bbb148ebad78f291
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-06 20:52:08 -0400

    changing versions

--- scripts/common/chromedriver.sh
+++ scripts/common/chromedriver.sh
@@ -5,7 +5,7 @@ BIN_DIR=$1
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Installs chromedriver for Linux 64 bit systems.
-  wget -N http://chromedriver.storage.googleapis.com/2.11/chromedriver_linux64.zip
+  wget -N http://chromedriver.storage.googleapis.com/2.12/chromedriver_linux64.zip
   unzip chromedriver_linux64.zip
   chmod +x chromedriver
   mv -f chromedriver $BIN_DIR
