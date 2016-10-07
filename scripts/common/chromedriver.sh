2e8e82a303b9fd52d327efc1c2795df6610a0238
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-07 19:03:14 -0400

    Trying messy version...

---
+++ scripts/common/chromedriver.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Scripts expect argument specifying bin dir.
+BIN_DIR=$1
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Installs chromedriver for Linux 64 bit systems.
+  wget -N http://chromedriver.storage.googleapis.com/2.24/chromedriver_linux64.zip
+  unzip chromedriver_linux64.zip
+  chmod +x chromedriver
+  mv -f chromedriver $BIN_DIR
+fi
