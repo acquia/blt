4b57db0c342fbc0e3c87713c66fc85510dc2bb6c
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-07 21:39:02 -0400

    Reducing verbosity of kill targets.

--- scripts/linux/xvbf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install and start xvfb.
-# @see http://tobyho.com/2015/01/09/headless-browser-testing-xvfb/
-sudo apt-get install xvfb
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
