d75357786478cf58e2640608fa922a6a2a81c53c
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-11 19:00:30 -0400

    Fixing Drupal VM integration with Selenium.

--- scripts/linux/install-chrome.sh
+++ scripts/linux/install-chrome.sh
@@ -10,6 +10,7 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Download google chrome.
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+  apt-get -f install
 
   # Installs chromedriver for Linux 64 bit systems.
   wget -N http://chromedriver.storage.googleapis.com/2.24/chromedriver_linux64.zip
