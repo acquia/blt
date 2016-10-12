f11d919a2bc2f6a303bf861045ff915ca2d987d7
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-12 19:11:12 -0400

    Fixing post-provision.sh.

--- scripts/drupal-vm/post-provision.sh
+++ scripts/drupal-vm/post-provision.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Append vendor/bin to $PATH via ~/.bashrc modification.
-grep -q -F 'export PATH=/var/www/$(hostname)/vendor/bin:$PATH' .bashrc || echo 'export PATH=/var/www/$(hostname)/vendor/bin:$PATH' >> .bashrc
+grep -q -F 'export PATH=/var/www/$(hostname -d | cut -d"." -f 1)/vendor/bin:$PATH' .bashrc || echo 'export PATH=/var/www/$(hostname -d | cut -d"." -f 1)/vendor/bin:$PATH' >> .bashrc
