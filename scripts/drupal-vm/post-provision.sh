c999e9c082c28b4ee63bb7b211f1f1ff33f51023
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-17 10:50:00 -0400

    Working on VM post provision.

--- scripts/drupal-vm/post-provision.sh
+++ scripts/drupal-vm/post-provision.sh
@@ -2,7 +2,7 @@
 
 set -x
 REPO_ROOT=/var/www/$(hostname -d | cut -d"." -f 1)
-cd $REPO_ROOT
+cd ${REPO_ROOT}
 
 # Add blt alias to front of .bashrc
 grep -q -F 'blt' ~/.bashrc || (cat ./vendor/acquia/blt/scripts/blt/alias ~/.bashrc > temp && mv temp ~/.bashrc)
