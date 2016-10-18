37757d99fa3c1447123e266decc48ccf03b26f2e
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-18 10:10:43 -0400

    Updating comments on post-provision.sh.

--- scripts/drupal-vm/post-provision.sh
+++ scripts/drupal-vm/post-provision.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -x
+# This command is run as sudo, '~' will expand to '/root'.
 VAGRANT_MACHINE_NAME=$(grep vagrant_machine_name: /vagrant/box/config.yml | cut -d' ' -f 2)
 REPO_ROOT=/var/www/${VAGRANT_MACHINE_NAME}
 cd ${REPO_ROOT}
 
-# Add blt alias to front of .bashrc
+# Add blt alias to front of .bashrc so that it applies to non-interactive shells.
 grep -q -F 'blt' /home/vagrant/.bashrc || (cat ./vendor/acquia/blt/scripts/blt/alias /home/vagrant/.bashrc > temp && mv temp /home/vagrant/.bashrc)
