890fa848145b710758a7bfbd1bbe3ff33f634097
Dane Powell <git@danepowell.com>
2016-11-08 13:04:38 -0800

    Added post-code-update hook.

---
+++ template/hooks/common/post-code-update/post-code-update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Cloud Hook: post-code-update
+#
+# The post-code-update hook runs in response to code commits. When you
+# push commits to a Git branch, the post-code-update hooks runs for
+# each environment that is currently running that branch.
+#
+# The arguments for post-code-update are the same as for post-code-deploy,
+# with the source-branch and deployed-tag arguments both set to the name of
+# the environment receiving the new code.
+#
+# post-code-update only runs if your site is using a Git repository. It does
+# not support SVN.
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+. /var/www/html/$site.$target_env/vendor/acquia/blt/scripts/cloud-hooks/functions.sh
+
+deploy_updates
+
+exit $status
