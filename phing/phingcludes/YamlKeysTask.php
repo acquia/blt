e3691e0cd5949b9f4ca78eba9c54df73364fccbf
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-12-20 12:42:10 -0500

    documentation and refactoring

--- phing/phingcludes/YamlKeysTask.php
+++ phing/phingcludes/YamlKeysTask.php
@@ -3,24 +3,29 @@
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * Class YamlVariableTask
+ * Class YamlKeysTask
  *
  * This task allows keys to be extracted from YAML variables and output to Phing
  * properties.
  */
 class YamlKeysTask extends Task {
 
+
+  /**
+   * The property string delimiter.
+   * @var string
+   */
   const PROP_DELIMITER = '.';
 
   /**
-   * The YAML file from which to read the variable.
+   * The YAML file from which to read the keys.
    * @var string
    */
   protected $file = NULL;
 
 
   /**
-   * The YAML variable name. This may be a nested array.
+   * The top-level YAML variable name.
    * @var string
    */
   protected $variable = NULL;
@@ -31,6 +36,10 @@ class YamlKeysTask extends Task {
    */
   protected $property = NULL;
 
+  /**
+   * Output list delimiter.
+   * @var string
+   */
   protected $listDelimiter = ',';
 
   /**
@@ -39,8 +48,7 @@ class YamlKeysTask extends Task {
    */
   private $outputProperty;
 
-  public function init() {
-  }
+  public function init() {}
 
   public function main() {
 
@@ -50,8 +58,8 @@ class YamlKeysTask extends Task {
     foreach ($parsed[$this->variable] as $key => $value) {
       $keys = array();
       if (!empty($this->property)) {
-        $propChain = explode(self::PROP_DELIMITER, $this->property);
-        $keys = $this->extractNestedKeys($value, $propChain);
+        $keyChain = explode(self::PROP_DELIMITER, $this->property);
+        $keys = $this->extractNestedKeys($value, $keyChain);
       }
       else {
         $keys[] = $key;
@@ -60,7 +68,7 @@ class YamlKeysTask extends Task {
       if (empty($keys)) {
         throw new BuildException("Could not locate " .
           $this->variable . "[" . $key . "]" .
-          (!empty($propChain) ? "[" . implode('][', $propChain) . "]" : '')
+          (!empty($keyChain) ? "[" . implode('][', $keyChain) . "]" : '')
         );
       }
 
@@ -80,7 +88,7 @@ class YamlKeysTask extends Task {
    * Sets $this->file property.
    *
    * @param string $file
-   *   The PHP file from which to read the variable.
+   *   The YAML file from which to read the variable.
    */
   public function setFile($file) {
     $this->file = $file;
@@ -90,16 +98,28 @@ class YamlKeysTask extends Task {
    * Sets $this->variable
    *
    * @param string $variable
-   *   The PHP variable name. This may be a nested array.
+   *   The top-level YAML variable name.
    */
   public function setVariable($variable) {
     $this->variable = $variable;
   }
 
+  /**
+   * Sets $this->property
+   *
+   * @param $property string
+   *  The nested property of $this->variable.
+   */
   public function setProperty($property) {
     $this->property = $property;
   }
 
+  /**
+   * Sets $this->listDelimiter
+   *
+   * @param $listDelimiter string
+   *    The character to use as a list item separator.
+   */
   public function setListDelimiter($listDelimiter) {
     $this->listDelimiter = $listDelimiter;
   }
@@ -107,23 +127,33 @@ class YamlKeysTask extends Task {
   /**
    * Sets $this->outputProperty property.
    *
-   * @param string $prop
+   * @param $prop string
    *   The name of the Phing property whose value to set.
    */
   public function setOutputProperty($prop) {
     $this->outputProperty = $prop;
   }
 
-  private function extractNestedKeys(array $arr, array $propChain) {
+  /**
+   * Returns the keys nested within an array under particular sequence of keys.
+   *
+   * @param array $arr
+   *  An array which contains a nested series of keys.
+   * @param $keyChain array
+   *  An array of keys to traverse.
+   * @return array
+   *  The extracted keys from $arr
+   */
+  private function extractNestedKeys(array $arr, array $keyChain) {
     $values = array();
     $index = 0;
     $curArrPos = $arr;
-    foreach ($propChain as $propKey) {
-      if (array_key_exists($propKey, $curArrPos)) {
-        $curArrPos = $curArrPos[$propKey];
+    foreach ($keyChain as $arrKey) {
+      if (array_key_exists($arrKey, $curArrPos)) {
+        $curArrPos = $curArrPos[$arrKey];
         // If we're at the requested depth, extract
         // the keys here.
-        if ($index === count($propChain) - 1) {
+        if ($index === count($keyChain) - 1) {
           foreach ($curArrPos as $key => $value) {
             $values[] = $key;
           }
