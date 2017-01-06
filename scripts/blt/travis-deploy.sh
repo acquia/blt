7398d09ab8329f5eeb88c1b74df097c18f860a80
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-06 15:52:17 -0500

    Fixes #649: Adding Pipelines build to BLT testing.

---
+++ scripts/blt/travis-deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script performs a deployment for BLT itself via Travis CI.
+
+# Deploy to Acquia Cloud
+cd ../blt-project
+blt deploy -Ddeploy.commitMsg="Automated commit by Travis CI for Build ${TRAVIS_BUILD_ID}" -Ddeploy.branch="8.x-build"
+# Execute functional tests to assert that deployment artifact was created correctly.
+phpunit tests/phpunit --group=deploy
+
+# Execute Pipelines build.
+# N3_KEY and N3_SECRET are Travis CI environmental variables.
+echo ~/.acquia/pipelines/credentials << EOF
+n3_endpoint: 'https://cloud.acquia.com'
+n3_key: ${N3_KEY}
+n3_secret: ${N3_SECRET}
+EOF
+cd deploy
+curl -o pipelines https://cloud.acquia.com/pipeline-client/download
+chmod a+x pipelines
+./pipelines start
