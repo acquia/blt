e8c04abc55968fa91f0cb09693f776d8cc3191e4
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-01-06 15:11:16 -0500

    Fixing BLT executable. (#933)

--- blt.sh
+++ blt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# @todo Move this into bin/blt directly.
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BIN=$(cd ${SCRIPT_DIR}/../../bin && pwd)
 PHING=${BIN}/phing
