c0072f83d69950be0f1b726ef056723480308eeb
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-09 16:02:24 -0500

    More fixes.

--- scripts/blt/release-blt.sh
+++ scripts/blt/release-blt.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
-# Usage ./scripts/blt/release-blt 8.6.11 [github-token]
+# Usage ./scripts/blt/release-blt [tag] [github-token]
+
+tag="$1"
+TOKEN="$2"
+
+set -x
 
 echo "This will perform a hard reset on the current repository and create a draft release for BLT."
 read -p "Press any key to continue."
 
 git clean -fd .
+git remote update
 git reset --hard
 git checkout 8.x
-git pull origin 8.x
-github_changelog_generator -t $TOKEN --future-release=$tag
+git reset --hard origin/8.x
+github_changelog_generator --token=${TOKEN} --future-release=$tag --output=CHANGELOG.partial
+
+# Remove last 3 lines from CHANGELOG.partial.
+head -n -3 CHANGELOG.partial > CHANGELOG.partial.trimmed ; mv CHANGELOG.partial.trimmed CHANGELOG.partial
+# Append CHANGELOG.md to CHANGELOG.partial.
+cat CHANGELOG.md >> CHANGELOG.partial
+# Replace CHANGELOG.md with CHANGELOG.partial contents.
+mv CHANGELOG.partial CHANGELOG.md
+
+git checkout 8.x-release
+git merge 8.x
+git push origin 8.x-release
 RELEASE_NOTES=$(cat ${SCRIPT_DIR}../../temp-changelog.md})
 API_JSON=$(printf '{"tag_name": "%s","target_commitish": "8.x-release","name": "v%s","body": " %s","draft": true,"prerelease": true}' $tag $tag $RELEASE_NOTES)
 curl --data "$API_JSON" https://api.github.com/repos/acquia/blt/releases?access_token=$TOKEN
--- scripts/blt/test-blt.sh
+++ scripts/blt/test-blt.sh
@@ -3,7 +3,6 @@
 # Usage ./scripts/blt/release-blt 8.6.11
 
 tag="$1"
-TOKEN="$2"
 branch=$tag-build
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -14,6 +13,7 @@ read -p "Press any key to continue."
 set -x
 cd $SCRIPT_DIR/../../../
 export COMPOSER_PROCESS_TIMEOUT=2000
+# @todo prompt to delete if exists
 rm -rf blted8
 composer create-project acquia/blt-project:8.x-dev blted8 --no-interaction
 cd blted8
@@ -22,32 +22,23 @@ cd blted8
 # echo '$databases["default"]["default"]["password"] = "drupal";' >> docroot/sites/default/settings/local.settings.php
 ./vendor/bin/blt local:setup
 cd docroot
-drush uli
 cd ..
+./vendor/bin/blt validate
 ./vendor/bin/blt tests
-read -p "Press any key to continue. This will destroy blted8 and re-create it, using a VM this time."
-cd ../
-
-rm -rf blted8
-mkdir blted8
-cd blted8
-git init
-composer init --stability=dev --no-interaction
-composer config prefer-stable true
-composer require acquia/blt:8.x-dev
-composer update
+read -p "Press any key to continue. This will create a VM and re-run tests there."
 ./vendor/bin/blt vm
-./vendor/bin/blt local:setup -v
-drush @blted8.local uli
+./vendor/bin/blt local:setup
 drush @blted8.local ssh blt tests:behat
 read -p "Press any key to continue. This will destroy the VM and attempt to perform a Pipelines build."
 vagrant destroy
 
-./vendor/bin/yaml-cli update:value git.remotes.0 bolt8pipeline@svn-2420.devcloud.hosting.acquia.com:bolt8pipeline.git
+./vendor/bin/yaml-cli update:value blt/project.yml git.remotes.0 bolt8pipeline@svn-2420.devcloud.hosting.acquia.com:bolt8pipeline.git
 ./vendor/bin/blt ci:pipelines:init
 git co -b ${branch}
 git add -A
-git commit -m "Creating test branch for BLT release ${tag}"
+git commit -m "BLT-000: Creating test branch for BLT release ${tag}"
 git remote add origin bolt8pipeline@svn-2420.devcloud.hosting.acquia.com:bolt8pipeline.git
 git push origin ${branch}
 pipelines start
+
+# @todo have pipelines deploy and install on ODE.
