baf64d50be1f8eab03965de23d71b6667d78b2fb
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-10 09:12:46 -0500

    Fixes

--- scripts/blt/release-blt.sh
+++ scripts/blt/release-blt.sh
@@ -5,11 +5,11 @@
 tag="$1"
 TOKEN="$2"
 
-set -x
-
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "This will perform a hard reset on the current repository and create a draft release for BLT."
 read -p "Press any key to continue."
 
+set -x
 git clean -fd .
 git remote update
 git reset --hard
@@ -19,6 +19,7 @@ github_changelog_generator --token=${TOKEN} --future-release=$tag --output=CHANG
 
 # Remove last 3 lines from CHANGELOG.partial.
 head -n -3 CHANGELOG.partial > CHANGELOG.partial.trimmed ; mv CHANGELOG.partial.trimmed CHANGELOG.partial
+RELEASE_NOTES=$(cat CHANGELOG.partial.trimmed)
 # Append CHANGELOG.md to CHANGELOG.partial.
 cat CHANGELOG.md >> CHANGELOG.partial
 # Replace CHANGELOG.md with CHANGELOG.partial contents.
@@ -27,6 +28,5 @@ mv CHANGELOG.partial CHANGELOG.md
 git checkout 8.x-release
 git merge 8.x
 git push origin 8.x-release
-RELEASE_NOTES=$(cat ${SCRIPT_DIR}../../temp-changelog.md})
-API_JSON=$(printf '{"tag_name": "%s","target_commitish": "8.x-release","name": "v%s","body": " %s","draft": true,"prerelease": true}' $tag $tag $RELEASE_NOTES)
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "8.x-release","name": "%s","body": "%s","draft": true,"prerelease": true}' $tag $tag $RELEASE_NOTES)
 curl --data "$API_JSON" https://api.github.com/repos/acquia/blt/releases?access_token=$TOKEN
