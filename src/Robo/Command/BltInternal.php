3492f061f1c6da24a13bbd68fba3ece86683691f
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-10 10:29:50 -0500

    Further porting.

--- src/Robo/Command/BltInternal.php
+++ src/Robo/Command/BltInternal.php
@@ -3,6 +3,7 @@
 namespace Acquia\Blt\Robo\Command;
 
 use Robo\Tasks;
+use GuzzleHttp\Client;
 
 /**
  * This is project's console commands configuration for Robo task runner.
@@ -21,14 +22,13 @@ class BltInternal extends Tasks
    * @param string $github_token A github access token
    *
    * @return int
-   *   The status code.
+   *   The CLI status code.
    */
   public function bltRelease($tag, $github_token)
   {
 
     $requirements_met = $this->checkCommandsExist([
       'git',
-      'github_changelog_generator',
     ]);
     if (!$requirements_met) {
       return 1;
@@ -37,24 +37,142 @@ class BltInternal extends Tasks
     $this->yell("This will destroy any uncommitted work on the current branch. It will also hard reset 8.x and 8.x-release to match the upstream history.");
     $continue = $this->confirm("Continue?");
 
-    if ($continue) {
+    if (!$continue) {
+      return 0;
+    }
       // Clean up all staged and unstaged files on current branch.
       $this->_exec('git clean -fd .');
       $this->_exec('git remote update');
       $this->_exec('git reset --hard');
 
-      // Reset to match upstream history of 8.x.
+      // Reset local 8.x to match upstream history of 8.x.
       $this->_exec('git checkout 8.x');
       $this->_exec('git reset --hard origin/8.x');
 
-      // Generate release notes.
-      $partial_changelog_filename = 'CHANGELOG.partial';
-      $this->_exec("github_changelog_generator --token=$github_token --future-release=$tag --output=$partial_changelog_filename");
-      $partial_changelog_contents = file_get_contents($partial_changelog_filename);
-      // Remove last 3 lines.
-      $trimmed_partial_changelog = implode("\n", array_slice(explode("\n", $partial_changelog_contents), -3));
-      print $trimmed_partial_changelog;
+      // Reset local 8.x-release to match upstream history of 8.x-release.
+      $this->_exec('git checkout 8.x-release');
+      $this->_exec('git reset --hard origin/8.x-release');
+
+      // Merge 8.x into 8.x-release and push.
+      $this->_exec('git merge 8.x');
+      $this->_exec('git push origin 8.x-release');
+
+      $partial_release_notes = $this->generateReleaseNotes($tag, $github_token);
+
+      $request_payload = [
+        'tag_name' => $tag,
+        'name' => $tag,
+        'target_commitish' => '8.x-release',
+        'body' => $partial_release_notes,
+        'draft' => true,
+        'prerelease' => true,
+      ];
+
+
+    $client = new Client([
+      // Base URI is used with relative requests
+      'base_uri' => 'https://api.github.com/repos/acquia/blt',
+      'query' => [
+        'access_token' => $github_token,
+      ],
+    ]);
+    $response = $client->request('POST', 'releases', [
+      'json' => $request_payload,
+    ]);
+  }
+
+  /**
+   * Update CHANGELOG.md with notes for new release.
+   *
+   * @param string $tag The tag name. E.g, 8.6.10
+   * @param string $github_token A github access token
+   *
+   * @return int
+   *   The CLI status code.
+   */
+  public function bltReleaseNotes($tag, $github_token) {
+
+    $requirements_met = $this->checkCommandsExist([
+      'github_changelog_generator',
+    ]);
+    if (!$requirements_met) {
+      return 1;
     }
+
+    $this->yell("You should execute this command on a clean, updated checkout of 8.x.");
+    $continue = $this->confirm("Continue?");
+
+    if (!$continue) {
+      return 0;
+    }
+
+    if (!$trimmed_partial_changelog = $this->generateReleaseNotes($tag, $github_token)) {
+      $this->yell("Failed to generate release notes");
+      return 1;
+    }
+
+    // Remove first 4 lines from full changelog.
+    $full_changelog_filename = 'CHANGELOG.md';
+    $full_changelog = file_get_contents($full_changelog_filename);
+    $trimmed_full_changelog = $this->trimStartingLines($full_changelog, 1);
+
+    $new_full_changelog = $trimmed_partial_changelog . $trimmed_full_changelog;
+    file_put_contents($full_changelog_filename, $new_full_changelog);
+
+    $this->say("$full_changelog_filename has been updated. Please commit and PUSH the changes.");
+
+    return 0;
+  }
+
+  /**
+   * Generate notes for new release.
+   *
+   * @param $tag
+   * @param $github_token
+   *
+   * @return int|string
+   *   FALSE on failure, otherwise the release notes.
+   */
+  protected function generateReleaseNotes($tag, $github_token) {
+    // Generate release notes.
+    $partial_changelog_filename = 'CHANGELOG.partial';
+    if (!$this->taskExec("github_changelog_generator --token=$github_token --future-release=$tag --output=$partial_changelog_filename")->run()->wasSuccessful()) {
+      $this->yell("Unable to generate CHANGELOG using github_changelog_generator.");
+      return 1;
+    }
+
+    // Remove last 3 lines from new, partial changelog.
+    $partial_changelog_contents = file_get_contents($partial_changelog_filename);
+    $trimmed_partial_changelog = $this->trimEndingLines($partial_changelog_contents, 3);
+    unlink($partial_changelog_filename);
+
+    return $trimmed_partial_changelog;
+  }
+
+  /**
+   * Trims the last $num_lines lines from end of a text string.
+   *
+   * @param string $text A string of text.
+   * @param int $num_lines The number of lines to trim from the end of the text.
+   *
+   * @return string
+   *   The trimmed text.
+   */
+  protected function trimEndingLines($text, $num_lines) {
+    return implode("\n", array_slice(explode("\n", $text), 0, sizeof($text) - $num_lines));
+  }
+
+  /**
+   * Trims the last $num_lines lines from beginning of a text string.
+   *
+   * @param string $text A string of text.
+   * @param int $num_lines The number of lines to trim from beginning of text.
+   *
+   * @return string
+   *   The trimmed text.
+   */
+  protected function trimStartingLines($text, $num_lines) {
+    return implode("\n", array_slice(explode("\n", $text), $num_lines));
   }
 
     /**
