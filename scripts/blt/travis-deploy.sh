90c396134c0940788807e71ac03626d0e748f192
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-16 10:22:10 -0500

    maybe

--- scripts/blt/travis-deploy.sh
+++ scripts/blt/travis-deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+BRANCH=$1
+
 # This script performs a deployment for BLT itself via Travis CI.
-cd $TRAVIS_BUILD_DIR/../blt-project
+cd ${TRAVIS_BUILD_DIR}/../blt-project
 
 # N3_KEY and N3_SECRET are Travis CI environmental variables.
 curl -o pipelines https://cloud.acquia.com/pipeline-client/download
@@ -9,13 +11,11 @@ chmod a+x pipelines
 ./pipelines configure --key=$N3_KEY --secret=$N3_SECRET
 
 # Deploy to Acquia Cloud
-blt deploy -Ddeploy.commitMsg="Automated commit to artifact by Travis CI for Build ${TRAVIS_BUILD_ID}" -Ddeploy.branch="8.x-build"
-# Execute functional tests to assert that deployment artifact was created correctly.
-phpunit tests/phpunit --group=deploy
+blt deploy -Ddeploy.commitMsg="Automated commit to artifact by Travis CI for Build ${TRAVIS_BUILD_ID}" -Ddeploy.branch="${BRANCH}-build"
 
 # Update source repo, then execute Pipelines build.
 git add -A
 git remote add acquia bolt8@svn-5223.devcloud.hosting.acquia.com:bolt8.git
 git commit -m "Automated commit to source by Travis CI for Build ${TRAVIS_BUILD_ID}". -n
-git push acquia 8.x
+git push acquia ${BRANCH}
 ./pipelines start
