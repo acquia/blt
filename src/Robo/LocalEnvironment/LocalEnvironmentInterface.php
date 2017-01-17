1a2873d1dbbb9ecf5f13c6ba17c72757eaeaa2ac
Matthew Grasmick <matt.grasmick@gmail.com>
2017-01-16 21:32:09 -0500

    Pausing robo work for the night.

--- src/Robo/LocalEnvironment/LocalEnvironmentInterface.php
+++ src/Robo/LocalEnvironment/LocalEnvironmentInterface.php
@@ -6,5 +6,10 @@ use Acquia\Blt\Robo\LocalEnvironment\LocalEnvironment;
 
 interface LocalEnvironmentInterface {
 
+  /**
+   * @param \Acquia\Blt\Robo\LocalEnvironment\LocalEnvironment $local_environment
+   *
+   * @return mixed
+   */
   public function setLocalEnvironment(LocalEnvironment $local_environment);
 }
