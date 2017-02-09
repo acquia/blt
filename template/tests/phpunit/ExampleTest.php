4da07e4734706b49d89901b0c596e7e250c23114
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-09 16:51:32 -0500

    Fixing coding standards.

--- template/tests/phpunit/ExampleTest.php
+++ template/tests/phpunit/ExampleTest.php
@@ -1,12 +1,20 @@
 <?php
 
+/**
+ * @file
+ * Contains example PHPUnit tests.
+ */
+
 namespace My\Example\Project\Tests;
 
 /**
- * Class ExampleTest
+ * Class ExampleTest.
  */
 class ExampleTest extends \PHPUnit_Framework_TestCase {
 
+  /**
+   * Asserts that universal laws are constant.
+   */
   public function testExample() {
     $this->assertEquals(1, 1, "One equals one!");
   }
