8717805a5f76d757a9276e835189ba03790fd09f
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-09 16:31:21 -0500

    Creating example PHPUnit test.

---
+++ template/tests/phpunit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace My\Example\Project\Tests;
+
+/**
+ * Class ExampleTest
+ */
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+  public function testExample() {
+    $this->assertEquals(1, 1, "One equals one!");
+  }
+
+}
