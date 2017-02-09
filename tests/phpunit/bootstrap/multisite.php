7e983674ed3fadb2f402fb37d3f44b5d4b43fb99
bobbygryzynger <bobby.gryzynger@gmail.com>
2017-02-08 23:24:04 -0500

    update test values

--- tests/phpunit/bootstrap/multisite.php
+++ tests/phpunit/bootstrap/multisite.php
@@ -8,7 +8,5 @@
 $_blt_site = 'subsite';
 $_blt_properties = [
   'site.name' => 'subsite',
-  'site.uri.ci' => '127.0.0.1:3000',
-  'site.drush.aliases.ci' => 'subsite.ci.local',
   'multisite.names' => 'default,subsite',
 ];
--- tests/phpunit/bootstrap/site.php
+++ tests/phpunit/bootstrap/site.php
@@ -8,6 +8,5 @@
 $_blt_properties = [
   'site.name' => 'default',
   'site.drush.aliases.ci' => 'self',
-  'site.uri.ci' => '127.0.0.1:8888',
   'multisite.names' => 'default',
 ];
