7faeb08e89b510d97f49dac106a19a3edf36db30
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-14 17:15:05 -0500

    Moving composer validation.

--- scripts/blt/ci/shared/run_tests.sh
+++ scripts/blt/ci/shared/run_tests.sh
@@ -2,9 +2,6 @@
 
 set -ev
 
-# Validate composer.json file.
-composer validate --no-check-all --ansi
-
 #  The local.hostname must be set to 127.0.0.1:8888 because we are using drush runserver to test the site.
 yaml-cli update:value blt/project.yml project.local.hostname '127.0.0.1:8888'
 
