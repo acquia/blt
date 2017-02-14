46828433b67327b3eaeb4f59aa5b3ea15ea42ac5
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-14 17:12:45 -0500

    Removing composer install from abstraction.

--- scripts/blt/ci/shared/setup_environment.sh
+++ scripts/blt/ci/shared/setup_environment.sh
@@ -3,7 +3,10 @@
 set -ev
 
 # Get the latest version of composer.
-composer self-update
-composer global require "hirak/prestissimo:^0.3"
+export PATH=${COMPOSER_BIN}:$PATH
+
+# Install proper version of node for front end tasks.
+nvm install 4.4.1
+nvm use 4.4.1
 
 set +v
