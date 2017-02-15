c25dd582c0870d046097b21e52849d938b48abcb
Matthew Grasmick <matt.grasmick@gmail.com>
2017-02-14 21:03:09 -0500

    Fixing composer symlink in sandbox branch.

--- scripts/blt/ci/internal/deploy_blted8.sh
+++ scripts/blt/ci/internal/deploy_blted8.sh
@@ -3,6 +3,10 @@
 set -ev
 
 cd ${TRAVIS_BUILD_DIR}/../blt-project
+# Remove the symlink definition for BLT from composer.json.
+composer config --unset repo.blt
+# If a new commit has been made to 8.x in the course of this build, we won't get the same version!
+composer update acquia/blt
 git remote add github git@github.com:acquia-pso/blted8.git
 git add -A
 git commit -m "Automated commit by Travis CI for Build ${TRAVIS_BUILD_ID}" -n
