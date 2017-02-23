804da669fe9671ba92105c46ee102a4b0de889ee
Thom Toogood <thomtoogood@gmail.com>
2017-02-23 19:48:17 +1100

    Added vm:bb:init command.

---
+++ scripts/beetbox/beetbox.aliases.drushrc.php
@@ -0,0 +1,11 @@
+// [vagrant_machine_name].local
+$aliases['${project.machine_name}.local'] = array(
+  'root' => '/var/beetbox/docroot',
+  // vagrant_hostname
+  'uri' => '${project.local.uri}',
+  // vagrant_hostname
+  'remote-host' => '${project.local.hostname}',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+);
+
