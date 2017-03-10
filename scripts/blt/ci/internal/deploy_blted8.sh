4946a791ce7a82a87c2d19bd556e706f465a058d
Matthew Grasmick <matt.grasmick@gmail.com>
2017-03-09 20:37:45 -0500

    Fixing deployment on BLTed8 sandbox.

--- scripts/blt/ci/internal/deploy_blted8.sh
+++ scripts/blt/ci/internal/deploy_blted8.sh
@@ -4,6 +4,7 @@ set -ev
 
 cd ${TRAVIS_BUILD_DIR}/../blt-project
 
+# git.remotes already defined in create_blt_project.sh.
 yaml-cli update:value .travis.yml addons.ssh_known_hosts.0 svn-5223.devcloud.hosting.acquia.com
 yaml-cli update:value .travis.yml before_deploy.0 'openssl aes-256-cbc -K $encrypted_065fa5839cf8_key -iv $encrypted_065fa5839cf8_iv -in id_rsa_blted8.enc -out ~/.ssh/id_rsa -d; chmod 600 ~/.ssh/id_rsa; eval "$(ssh-agent -s)"; ssh-add ~/.ssh/id_rsa;'
 
@@ -12,10 +13,10 @@ composer config --unset repo.blt
 composer require acquia/blt:8.x-dev#${TRAVIS_COMMIT}
 composer update --lock
 git remote add github git@github.com:acquia-pso/blted8.git
-echo "[![Build Status](https://travis-ci.org/acquia-pso/blted8.svg?branch=master)](https://travis-ci.org/acquia-pso/blted8)" >> README.md
+echo "[![Build Status](https://travis-ci.org/acquia-pso/blted8.svg?branch=8.x)](https://travis-ci.org/acquia-pso/blted8)" >> README.md
 
 git add -A
-git commit -m "Automated commit by Travis CI for Build ${TRAVIS_BUILD_ID}" -n
+git commit -m "Automated commit for BLT repo by Travis CI for Build ${TRAVIS_BUILD_ID}" -n
 git checkout -b ${TRAVIS_BRANCH}
 git push github ${TRAVIS_BRANCH} -f
 
