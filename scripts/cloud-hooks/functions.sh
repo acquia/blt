e75d45ba48231052a243b0fc23663a668502e5ba
Lindsey Catlett <lindsey.catlett@gmail.com>
2017-03-20 09:30:06 -0400

    Add Cloud hooks for ACSF  (#1209)
    * Add ACSF drush utility commands.
    
    * Add support for multisite cloud hooks and BLT deploy tasks on ACSF.
    
    * Enable BLT deploy updates on ACSF by default.
    
    * Documenting post-drupal-scaffold-cmd to applying patches (#1208)
    
    Plus minor grammer change to "Contributing.md" :
    
    are -> our
    
    Signed-off-by: Justin Winter <justinlevi@gmail.com>
    
    * Updating README.md for patches.

--- scripts/cloud-hooks/functions.sh
+++ scripts/cloud-hooks/functions.sh
@@ -3,7 +3,52 @@
 
 status=0
 
+drush_alias=${site}'.'${target_env}
+
 deploy_updates() {
+
+  case $target_env in
+    [01]*)
+      acsf_deploy
+      ;;
+    *)
+      ace_deploy
+      ;;
+    esac
+}
+
+
+acsf_deploy() {
+  sites=()
+  # Prep for BLT commands.
+  repo_root="/var/www/html/$site.$target_env"
+  export PATH=$repo_root/vendor/bin:$PATH
+  cd $repo_root
+
+  echo "Running updates for environment: $target_env"
+
+  # Generate an array of all site URIs on the Factory from parsed output of Drush utility. 
+  while IFS=$'\n' read -r line; do 
+      sites[i++]="$line"
+      done < <(drush @"${drush_alias}" --include=./drush acsf-tools-list | grep domains: -A 1 | grep 0: | sed -e 's/^[0: ]*//')
+      unset IFS
+
+  # Loop through each available site uri and run BLT deploy updates.
+  for uri in "${sites[@]}"; do
+  #Override BLT default deploy uri. 
+  blt deploy:update  -Denvironment=$target_env -Ddrush.uri="$uri"
+  if [ $? -ne 0 ]; then
+      echo "Update errored."
+      status=1;
+  fi
+
+  echo "Finished updates for site: $uri."
+  done
+
+  echo "Finished updates for all $target_env sites."
+}
+
+ace_deploy() {
 
   echo "Running updates for environment: $target_env"
 
@@ -20,7 +65,7 @@ deploy_updates() {
 
   echo "Finished updates for environment: $target_env"
 }
-
+  
 deploy_install() {
 
   echo "Installing site for environment: $target_env"
--- template/hooks/common/post-code-deploy/post-code-deploy.sh
+++ template/hooks/common/post-code-deploy/post-code-deploy.sh
@@ -17,11 +17,8 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
-acsf_file="/mnt/files/$AH_SITE_GROUP.$AH_SITE_ENVIRONMENT/files-private/sites.json"
-if [ ! -f $acsf_file ]; then
+
   . /var/www/html/$site.$target_env/vendor/acquia/blt/scripts/cloud-hooks/functions.sh
   deploy_updates
   # Send notifications to Slack, if configured. See readme/deploy.md for setup instructions.
   . `dirname $0`/../slack.sh
-  exit $status
-fi
