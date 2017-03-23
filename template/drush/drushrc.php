827f094cce3e9b6ab399835d528826e0ce291fc8
Malik Kotob <malikkotob@users.noreply.github.com>
2017-03-23 09:47:20 -0400

    Add note referencing upstream drushrc.php.

--- template/drush/drushrc.php
+++ template/drush/drushrc.php
@@ -4,3 +4,5 @@
 require __DIR__ . '/../vendor/acquia/blt/drush/drushrc.php';
 
 // Add project-specific code below.
+// @see https://github.com/acquia/blt/tree/8.x/drush/drushrc.php For examples
+// of valid statements for a Drush runtime config (drushrc) file.
