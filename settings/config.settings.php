40141ea362fc6743302d98e6be875ee20cac7cd4
Matthew Grasmick <matt.grasmick@gmail.com>
2017-03-30 11:26:11 -0400

    Adding env var to indicate active config key.

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -6,20 +6,28 @@ $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.stage']['status'] = FALSE;
 $config['config_split.config_split.prod']['status'] = FALSE;
 $config['config_split.config_split.ci']['status'] = FALSE;
+$_ENV['blt']['config_split']['key'] = '';
+
 if ($is_local_env) {
   if (getenv('TRAVIS')) {
     $config['config_split.config_split.ci']['status'] = TRUE;
+    $_ENV['blt']['config_split']['key'] = 'ci';
   }
   else {
     $config['config_split.config_split.local']['status'] = TRUE;
+    $_ENV['blt']['config_split']['key'] = 'local';
   }
 }
 elseif ($is_ah_dev_env) {
   $config['config_split.config_split.dev']['status'] = TRUE;
+  $_ENV['blt']['config_split']['key'] = 'dev';
 }
 elseif ($is_ah_stage_env) {
   $config['config_split.config_split.stage']['status'] = TRUE;
+  // @todo Use test or stage depending on context.
+  $_ENV['blt']['config_split']['key'] = 'stage';
 }
 elseif ($is_ah_prod_env) {
   $config['config_split.config_split.prod']['status'] = TRUE;
+  $_ENV['blt']['config_split']['key'] = 'prod';
 }
