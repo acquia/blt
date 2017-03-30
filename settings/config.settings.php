1c3e2abc17d9848627cadb8be26a5bbc1912030a
Matthew Grasmick <matt.grasmick@gmail.com>
2017-03-30 12:25:25 -0400

    Default cm.strategy to none.

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -6,21 +6,16 @@ $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.stage']['status'] = FALSE;
 $config['config_split.config_split.prod']['status'] = FALSE;
 $config['config_split.config_split.ci']['status'] = FALSE;
-$_ENV['blt']['config_split']['key'] = '';
 
 if ($is_local_env) {
   if (getenv('TRAVIS')) {
     $config['config_split.config_split.ci']['status'] = TRUE;
-    $_ENV['blt']['config_split']['key'] = 'ci';
   }
   else {
     $config['config_split.config_split.local']['status'] = TRUE;
-    $_ENV['blt']['config_split']['key'] = 'local';
   }
 }
 else {
-  $_ENV['blt']['config_split']['key'] = $_ENV['AH_SITE_ENVIRONMENT'];
-
   if ($is_ah_dev_env) {
     $config['config_split.config_split.dev']['status'] = TRUE;
   }
