3145bd6a5dcb9932db4b1ecb0c966662c512a16b
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-04-04 13:12:51 -0400

    Changing include order of cloud settings files. (#1310)

--- settings/blt.settings.php
+++ settings/blt.settings.php
@@ -46,13 +46,30 @@ $acsf_db_name = $is_acsf ? $GLOBALS['gardens_site_settings']['conf']['acsf_db_na
 $is_local_env = !$is_ah_env;
 
 /*******************************************************************************
+ * Acquia Cloud settings.
+ ******************************************************************************/
+
+if ($is_ah_env) {
+  if (!$is_acsf && file_exists('/var/www/site-php') && $site_dir == 'default') {
+    require "/var/www/site-php/{$_ENV['AH_SITE_GROUP']}/{$_ENV['AH_SITE_GROUP']}-settings.inc";
+  }
+
+  // Store API Keys and things outside of version control.
+  // @see settings/sample-secrets.settings.php for sample code.
+  $secrets_file = sprintf('/mnt/gfs/%s.%s/secrets.settings.php', $_ENV['AH_SITE_GROUP'], $_ENV['AH_SITE_ENVIRONMENT']);
+  if (file_exists($secrets_file)) {
+    require $secrets_file;
+  }
+}
+
+/*******************************************************************************
  * BLT includes & BLT default configuration.
  ******************************************************************************/
 
 // Includes caching configuration.
 require __DIR__ . '/cache.settings.php';
 
-// Includes configuratoin management settings.
+// Includes configuration management settings.
 require __DIR__ . '/config.settings.php';
 
 // Includes logging configuration.
@@ -105,22 +122,6 @@ if (file_exists($deploy_id_file)) {
 /*******************************************************************************
  * Environment-specific includes.
  ******************************************************************************/
-
-/**
- * Acquia Cloud settings.
- */
-if ($is_ah_env) {
-  if (!$is_acsf && file_exists('/var/www/site-php') && $site_dir == 'default') {
-    require "/var/www/site-php/{$_ENV['AH_SITE_GROUP']}/{$_ENV['AH_SITE_GROUP']}-settings.inc";
-  }
-
-  // Store API Keys and things outside of version control.
-  // @see settings/sample-secrets.settings.php for sample code.
-  $secrets_file = sprintf('/mnt/gfs/%s.%s/secrets.settings.php', $_ENV['AH_SITE_GROUP'], $_ENV['AH_SITE_ENVIRONMENT']);
-  if (file_exists($secrets_file)) {
-    require $secrets_file;
-  }
-}
 
 /**
  * Include optional site specific includes file.
