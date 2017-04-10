5c60058fc3b5331fba2e96f0064835ef80e9a5b5
Jeff Geerling <geerlingguy@mac.com>
2017-04-10 16:37:55 -0500

    Fixes #1355: Make Drush alias for 'local' work in VM guest or host by default.

--- scripts/drupal-vm/drupal-vm.aliases.drushrc.php
+++ scripts/drupal-vm/drupal-vm.aliases.drushrc.php
@@ -1,11 +1,16 @@
 // [vagrant_machine_name].local
 $aliases['${project.machine_name}.local'] = array(
-  // /var/www/[vagrant_machine_name]/docroot
+  // /var/www/${project.machine_name}/docroot
   'root' => '/var/www/${project.machine_name}/docroot',
   // vagrant_hostname
   'uri' => '${project.local.uri}',
-  // vagrant_hostname
-  'remote-host' => '${project.local.hostname}',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
-);
+  );
+// Add connection options if alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['${project.machine_name}.local'] += array(
+    // vagrant_hostname
+    'remote-host' => '${project.local.hostname}',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
