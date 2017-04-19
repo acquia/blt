4704f37da0b2ab527c622ae828afc87d0fc3d785
Matthew Grasmick <matt.grasmick@gmail.com>
2017-04-19 16:16:25 -0400

    Fixing CloudHooksCommand.

--- src/Robo/Commands/Setup/CloudHooksCommand.php
+++ src/Robo/Commands/Setup/CloudHooksCommand.php
@@ -16,8 +16,7 @@ class CloudHooksCommand extends BltTasks {
    */
   public function copy() {
     $this->taskCopyDir([
-      $this->getConfigValue('blt.root') . '/scripts/cloud-hooks/hooks',
-      $this->getConfigValue('repo.root') . '/hooks',
+      $this->getConfigValue('blt.root') . '/scripts/cloud-hooks/hooks' => $this->getConfigValue('repo.root') . '/hooks',
     ])
       ->run();
   }
