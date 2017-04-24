4fef683d0903fd250030efcbba06587c54c6a453
Matthew Grasmick <matt.grasmick@gmail.com>
2017-04-24 10:37:45 -0400

    Improving DR VM integration.

--- src/Robo/Commands/Blt/DoctorCommand.php
+++ src/Robo/Commands/Blt/DoctorCommand.php
@@ -11,27 +11,80 @@ use Symfony\Component\Yaml\Yaml;
 class DoctorCommand extends BltTasks {
 
   /**
+   * Inspects your local blt configuration for possible issues.
+   *
    * @command doctor
    */
   public function doctor() {
 
-    if ($this->getInspector()->isDrupalVmLocallyInitialized()) {
-      $drupal_vm_config = Yaml::parse(file_get_contents($this->getConfigValue('repo.root') . '/box/config.yml'));
-      $repo_root = $drupal_vm_config['vagrant_synced_folders'][0]['destination'];
-      $this->say("Drupal VM was detected. Running blt doctor inside of VM...");
-      $result = $this->taskExec("vagrant exec '$repo_root/vendor/bin/drush cc drush && ./vendor/bin/drush --include=$repo_root/vendor/acquia/blt/drush blt-doctor -r $repo_root/docroot'")
-        ->dir($this->getConfigValue('repo.root'))
-        ->detectInteractive()
-        ->run();
+    if ($this->getInspector()->isDrupalVmLocallyInitialized() && $this->getInspector()->isDrupalVmBooted()) {
+      $result = $this->executeDoctorInsideVm();
+      if ($result->wasSuccessful()) {
+        return $result;
+      }
     }
 
+    // Try BLT doctor with default alias. This might be a Drupal VM alias.
+    $alias = $this->getConfigValue('drush.alias');
+    $result = $this->executeDoctorOnHost($alias);
+
+    // If default alias failed, try again using @self alias.
+    if (!$result->wasSuccessful() && $alias != 'self') {
+      $this->logger->warning("Unable to run the doctor using @$alias. Trying with @self...");
+      $this->executeDoctorOnHost('self');
+    }
+
+    return $result;
+  }
+
+  /**
+   * Executes `blt doctor` inside Drupal VM.
+   *
+   * @return \Robo\Result
+   *   The command result.
+   */
+  protected function executeDoctorInsideVm() {
+    $drupal_vm_config = Yaml::parse(file_get_contents($this->getConfigValue('repo.root') . '/box/config.yml'));
+    $repo_root = $drupal_vm_config['vagrant_synced_folders'][0]['destination'];
+    $this->say("Drupal VM was detected. Running blt doctor inside of VM...");
+    $command = "cd $repo_root && $repo_root/vendor/bin/drush cc drush && $repo_root/vendor/bin/drush --include=$repo_root/vendor/acquia/blt/drush blt-doctor -r $repo_root/docroot";
+
+    return $this->executeCommandInDrupalVm($command);
+  }
+
+  /**
+   * Executes a command inside of Drupal VM.
+   *
+   * @param string $command
+   *   The command to execute.
+   *
+   * @return \Robo\Result
+   *   The command result.
+   */
+  protected function executeCommandInDrupalVm($command) {
+    $result = $this->taskExec("vagrant exec '$command'")
+      ->dir($this->getConfigValue('repo.root'))
+      ->detectInteractive()
+      ->run();
+
+    return $result;
+  }
+
+  /**
+   * Executes `blt doctor` on host machine.
+   *
+   * @return \Robo\Result
+   *   The command result.
+   */
+  protected function executeDoctorOnHost($alias) {
     $drush_bin = $this->getConfigValue('composer.bin') . '/drush';
     $include_dir = $this->getConfigValue('blt.root') . '/drush';
-    $alias = $this->getConfigValue('drush.alias');
-    $this->taskExec("$drush_bin @$alias --include=$include_dir blt-doctor")
+    $result = $this->taskExec("$drush_bin @$alias --include=$include_dir blt-doctor")
       ->dir($this->getConfigValue('docroot'))
       ->detectInteractive()
       ->run();
+
+    return $result;
   }
 
 }
