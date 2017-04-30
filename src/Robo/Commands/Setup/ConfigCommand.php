5d708bde6f49dbd2bb8638346bc0977f97bd81b4
Matthew Grasmick <matt.grasmick@gmail.com>
2017-04-29 20:49:48 -0400

    Refactoring ConfigCommand to use taskDrush().

--- src/Robo/Commands/Setup/ConfigCommand.php
+++ src/Robo/Commands/Setup/ConfigCommand.php
@@ -31,16 +31,14 @@ class ConfigCommand extends BltTasks {
 
     if ($strategy != 'none') {
       $this->invokeHook('pre-config-import');
-      $drush_alias = $this->getConfigValue('drush.alias');
 
-      $task = $this->taskExecStack()
-        ->dir($this->getConfigValue('docroot'))
+      $task = $this->taskDrush()
         // Sometimes drush forgets where to find its aliases.
-        ->exec("drush cc drush --yes")
-        ->exec("drush @$drush_alias pm-enable config --yes")
+        ->drush("cc drush --yes")
+        ->drush("pm-enable config --yes")
         // Rebuild caches in case service definitions have changed.
         // @see https://www.drupal.org/node/2826466
-        ->exec("drush @$drush_alias cache-rebuild")
+        ->drush("cache-rebuild")
         // Execute db updates.
         // This must happen before features are imported or configuration is
         // imported. For instance, if you add a dependency on a new extension to
@@ -48,23 +46,23 @@ class ConfigCommand extends BltTasks {
         // update hook before attempting to import the configuration.
         // If a db update relies on updated configuration, you should import the
         // necessary configuration file(s) as part of the db update.
-        ->exec("drush @$drush_alias updb --yes");
+        ->drush("updb --yes");
 
       switch ($strategy) {
         case 'core-only':
-          $this->importCoreOnly($task, $drush_alias, $cm_core_key);
+          $this->importCoreOnly($task, $cm_core_key);
           break;
 
         case 'config-split':
-          $this->importConfigSplit($task, $drush_alias);
+          $this->importConfigSplit($task);
           break;
 
         case 'features':
-          $this->importFeatures($task, $drush_alias, $cm_core_key);
+          $this->importFeatures($task, $cm_core_key);
           break;
       }
 
-      $task->exec("drush @$drush_alias cache-rebuild");
+      $task->drush("cache-rebuild");
       $task->run();
 
       $this->checkFeaturesOverrides();
@@ -72,8 +70,7 @@ class ConfigCommand extends BltTasks {
       // Check for configuration overrides.
       if (!$this->getConfigValue('cm.allow-overrides')) {
         $this->say("Checking for config overrides...");
-        $config_overrides = $this->taskExec("drush @$drush_alias cex sync -n");
-        $config_overrides->dir($this->getConfigValue('docroot'));
+        $config_overrides = $this->taskDrush()->drush("cex sync -n");
         if (!$config_overrides->run()->wasSuccessful()) {
           throw new \Exception("Configuration in the database does not match configuration on disk. You must re-export configuration to capture the changes. This could also indicate a problem with the import process, such as changed field storage for a field with existing content.");
         }
@@ -87,50 +84,48 @@ class ConfigCommand extends BltTasks {
   /**
    * Import configuration using core config management only.
    *
-   * @param $task
-   * @param $drush_alias
-   * @param $cm_core_key
+   * @param \Acquia\Blt\Robo\Tasks\DrushTask $task
+   * @param string $cm_core_key
    */
-  protected function importCoreOnly($task, $drush_alias, $cm_core_key) {
+  protected function importCoreOnly($task, $cm_core_key) {
     if (file_exists($this->getConfigValue("cm.core.dirs.$cm_core_key.path") . '/core.extension.yml')) {
-      $task->exec("drush @$drush_alias config-import $cm_core_key --yes");
+      $task->drush("config-import $cm_core_key --yes");
     }
   }
 
   /**
    * Import configuration using config_split module.
    *
-   * @param $task
+   * @param \Acquia\Blt\Robo\Tasks\DrushTask $task
    * @param $drush_alias
    */
-  protected function importConfigSplit($task, $drush_alias) {
+  protected function importConfigSplit($task) {
     // We cannot use ${cm.core.dirs.${cm.core.key}.path} here because
     // cm.core.key may be 'vcs', which does not have a path defined in
     // BLT config. Perhaps this should be refactored.
     $core_config_file = $this->getConfigValue('docroot') . '/' . $this->getConfigValue('cm.core.dirs.sync.path') . '/core.extension.yml';
     if (file_exists($core_config_file)) {
-      $task->exec("drush @$drush_alias pm-enable config_split --yes");
-      $task->exec("drush @$drush_alias config-import sync --yes");
+      $task->drush("pm-enable config_split --yes");
+      $task->drush("config-import sync --yes");
     }
   }
 
   /**
    * Import configuration using features module.
-   * @param $task
-   * @param $drush_alias
+   * @param \Acquia\Blt\Robo\Tasks\DrushTask $task
    * @param $cm_core_key
    */
-  protected function importFeatures($task, $drush_alias, $cm_core_key) {
-    $task->exec("drush @$drush_alias config-import $cm_core_key --partial --yes");
+  protected function importFeatures($task, $cm_core_key) {
+    $task->drush("config-import $cm_core_key --partial --yes");
     if ($this->getConfig()->has('cm.features.bundle"')) {
-      $task->exec("drush @$drush_alias pm-enable features --yes");
+      $task->drush("pm-enable features --yes");
       // Clear drush caches to register features drush commands.
-      $task->exec("drush cc drush --yes");
+      $task->drush("drush cc drush --yes");
       foreach ($this->getConfigValue('cm.features.bundle') as $bundle) {
-        $task->exec("drush @$drush_alias features-revert-all --bundle=$bundle --yes");
+        $task->drush("features-revert-all --bundle=$bundle --yes");
         // Revert all features again!
         // @see https://www.drupal.org/node/2851532
-        $task->exec("drush @$drush_alias features-revert-all --bundle=$bundle --yes");
+        $task->drush("features-revert-all --bundle=$bundle --yes");
       }
     }
   }
@@ -146,15 +141,13 @@ class ConfigCommand extends BltTasks {
     if ($this->getConfigValue('cm.features.no-overrides')) {
       $this->say("Checking for features overrides...");
       if ($this->getConfig()->has('cm.features.bundle')) {
-        $task = $this->taskExec()
-          ->dir($this->getConfigValue('docroot'));
-        $drush_alias = $this->getConfigValue('drush.alias');
+        $task = $this->taskDrush();
         foreach ($this->getConfigValue('cm.features.bundle') as $bundle) {
-          $task->exec("drush @$drush_alias fl --bundle=$bundle --format=json");
+          $task->drush("fl --bundle='$bundle' --format=json");
           $result = $task->printOutput(TRUE)->run();
           $output = $result->getOutputData();
-          $features_overriden = preg_match('/(changed|conflicts|added)( *)$/', $output);
-          if ($features_overriden) {
+          $features_overridden = preg_match('/(changed|conflicts|added)( *)$/', $output);
+          if ($features_overridden) {
             throw new \Exception("A feature in the $bundle bundle is overridden. You must re-export this feature to incorporate the changes.");
           }
         }
