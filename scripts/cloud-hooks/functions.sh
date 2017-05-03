0c6d73d8cb9f978b2c4a6c3a598c7c86df3bb247
Matthew Grasmick <matt.grasmick@gmail.com>
2017-05-02 21:46:44 -0400

    Fixing AC hooks.

--- scripts/cloud-hooks/functions.sh
+++ scripts/cloud-hooks/functions.sh
@@ -35,7 +35,7 @@ acsf_deploy() {
   # Loop through each available site uri and run BLT deploy updates.
   for uri in "${sites[@]}"; do
   #Override BLT default deploy uri.
-  blt deploy:update  -Denvironment=$target_env -Ddrush.uri="$uri" -Dblt.verbose=true
+  blt deploy:update  -D environment $target_env -D drush.uri "$uri" -v
   if [ $? -ne 0 ]; then
       echo "Update errored for site $uri."
       exit 1
@@ -56,7 +56,7 @@ ace_deploy() {
   export PATH=$repo_root/vendor/bin:$PATH
   cd $repo_root
 
-  blt deploy:update -Denvironment=$target_env -Dblt.verbose=true
+  blt deploy:update -D environment $target_env -v
   if [ $? -ne 0 ]; then
       echo "Update errored."
       exit 1
@@ -74,7 +74,7 @@ deploy_install() {
   export PATH=$repo_root/vendor/bin:$PATH
   cd $repo_root
 
-  blt deploy:drupal:install -Denvironment=$target_env -Dblt.verbose=true
+  blt deploy:drupal:install -D environment=$target_env -v
   if [ $? -ne 0 ]; then
       echo "Install errored."
       exit 1
