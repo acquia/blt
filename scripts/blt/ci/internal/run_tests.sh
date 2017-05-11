2749c114328ef17c3588cb792a9216aa3b3358e6
Matthew Grasmick <matt.grasmick@gmail.com>
2017-05-10 21:58:15 -0400

    Add --yes to setup command.

--- scripts/blt/ci/internal/run_tests.sh
+++ scripts/blt/ci/internal/run_tests.sh
@@ -7,7 +7,7 @@ yaml-cli update:value blt/project.yml project.local.hostname '127.0.0.1:8888'
 # Build codebase, validate, install Drupal, run basic tests.
 yaml-cli update:value blt/project.yml cm.strategy none
 blt validate:all
-blt setup -Dcreate_alias=false -Denvironment=ci
+blt setup --define environment=ci --yes
 blt tests:all --define behat.run-server=true --yes
 blt tests:behat:definitions
 drush config-export --root=docroot -y
