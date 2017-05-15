4fa56da11e51f3cd45e0aee9e21585007f3a6fad
Matthew Grasmick <matt.grasmick@gmail.com>
2017-05-15 17:08:26 -0400

    Fixing merge.

--- scripts/blt/ci/internal/run_tests.sh
+++ scripts/blt/ci/internal/run_tests.sh
@@ -6,7 +6,7 @@ yaml-cli update:value blt/project.yml project.local.hostname '127.0.0.1:8888'
 
 # Build codebase, validate, install Drupal, run basic tests.
 yaml-cli update:value blt/project.yml cm.strategy none
-blt validate:all -vv
+blt validate:all
 blt setup --define environment=ci --yes
 blt tests:all --define behat.run-server=true --yes
 blt tests:behat:definitions
