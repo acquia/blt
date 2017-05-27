ada1b39d1c2b9273876cc36de39ef7bc05fada9e
Matthew Grasmick <matt.grasmick@gmail.com>
2017-05-26 21:18:07 -0400

    Adding ability to update blt alias.

--- src/Robo/Commands/Blt/UpdateCommand.php
+++ src/Robo/Commands/Blt/UpdateCommand.php
@@ -117,8 +117,94 @@ class UpdateCommand extends BltTasks {
         $this->say("The <comment>blt</comment> alias was not installed.");
       }
     }
-    // @todo Compare BLT alias in .bash_profile with most current one.
-    // Prompt to update if outdated.
+    elseif (!$this->isBltAliasUpToDate()) {
+      $this->logger->warning("Your BLT alias is out of date. ");
+      $confirm = $this->confirm("Would you like to update it?");
+      if ($confirm) {
+        $this->updateAlias();
+      }
+    }
+  }
+
+  /**
+   * Checks if the installed alias is up-to-date.
+   *
+   * @return bool
+   *   TRUE if the installed alias is up to date.
+   */
+  protected function isBltAliasUpToDate() {
+    $installed_alias = $this->getInstalledAlias();
+    $canonical_alias = file_get_contents($this->getConfigValue('repo.root') . '/scripts/blt/alias');
+
+    return $installed_alias['alias'] === $canonical_alias;
+  }
+
+  /**
+   * Gets information about the installed BLT alias.
+   *
+   * @return array
+   *   An array of information about the installed BLT alias.
+   */
+  protected function getInstalledAlias() {
+    $alias_length = NULL;
+    $alias = NULL;
+    $config_file = $this->getInspector()->getCliConfigFile();
+    $contents = file_get_contents($config_file);
+    $begin_alias_pos = strpos($contents, 'function blt() {');
+    $end_alias_pos = $this->getClosingBracketPosition($contents, $begin_alias_pos);
+
+    if (!is_null($end_alias_pos)) {
+      $alias_length = $end_alias_pos - $begin_alias_pos;
+      $alias = substr($contents, $begin_alias_pos, $alias_length);
+    }
+
+    return [
+      'config_file' => $config_file,
+      'contents' => $contents,
+      'start_pos' => $begin_alias_pos,
+      'end_pos' => $end_alias_pos,
+      'length' => $alias_length,
+      'alias' => $alias,
+    ];
+  }
+
+  /**
+   * Replaces installed alias with up-to-date alias.
+   */
+  protected function updateAlias() {
+    $installed_alias = $this->getInstalledAlias();
+    $canonical_alias = file_get_contents($this->getConfigValue('repo.root') . '/scripts/blt/alias');
+    substr_replace($installed_alias['config_file'], $canonical_alias, $installed_alias['start_pos'],
+      $installed_alias['length']);
+  }
+
+  /**
+   * Find the position of a closing bracket for a given stanza in a string.
+   *
+   * @param $contents
+   *   The string containing the brackets.
+   * @param int $start_pos
+   *   The position of the opening bracket in the string that should be matched.
+   *
+   * @return int|NULL
+   *
+   */
+  protected function getClosingBracketPosition($contents, $start_pos) {
+    $brackets = ['{'];
+    for ($pos = $start_pos; $pos < strlen($contents); $pos++) {
+      $char = substr($contents, $pos, 1);
+      if ($char == '{') {
+        array_push($brackets, $char);
+      }
+      elseif ($char == '}') {
+        array_pop($brackets);
+      }
+      if (count($brackets) == 0) {
+        return $pos;
+      }
+    }
+
+    return NULL;
   }
 
   /**
