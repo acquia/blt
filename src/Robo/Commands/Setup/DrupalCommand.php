ab34f974b46150329ce4b551cf6f4885384e4297
Alex Knoll <arknoll@gmail.com>
2017-06-13 15:57:55 -0600

    Fix drush site install command to disable update status module. (#1663)

--- src/Robo/Commands/Setup/DrupalCommand.php
+++ src/Robo/Commands/Setup/DrupalCommand.php
@@ -36,6 +36,7 @@ class DrupalCommand extends BltTasks {
       ->drush("site-install")
       ->arg($this->getConfigValue('project.profile.name'))
       ->rawArg("install_configure_form.update_status_module='array(FALSE,FALSE)'")
+      ->rawArg("install_configure_form.enable_update_status_module=NULL")
       ->option('site-name', $this->getConfigValue('project.human_name'))
       ->option('site-mail', $this->getConfigValue('drupal.account.mail'))
       ->option('account-name', $username, '=')
