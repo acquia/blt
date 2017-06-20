a5f4b8654ede323acfbf5ea628c14ff0010a006d
Matthew Grasmick <matt.grasmick@gmail.com>
2017-06-20 15:09:27 -0400

    Removing default for phantomjs in dvm.

--- src/Robo/Commands/Vm/VmCommand.php
+++ src/Robo/Commands/Vm/VmCommand.php
@@ -177,7 +177,6 @@ class VmCommand extends BltTasks {
     $contents = Yaml::parse(file_get_contents($filename));
     $contents['drush']['default_alias'] = $this->drupalVmAlias;
     $contents['drush']['aliases']['local'] = $this->drupalVmAlias;
-    $contents['behat']['web-driver'] = 'phantomjs';
     $yaml = Yaml::dump($contents, 3, 2);
     file_put_contents($filename, $yaml);
 
