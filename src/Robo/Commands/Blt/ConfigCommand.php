89956278c19fbb0d8075a1d41a45096e0b09ce3a
Matthew Grasmick <matt.grasmick@gmail.com>
2017-06-21 14:13:18 -0400

    Connects to #1711: Adding config:get and config:dump commmands.

---
+++ src/Robo/Commands/Blt/ConfigCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acquia\Blt\Robo\Commands\Blt;
+
+use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Exceptions\BltException;
+
+/**
+ * Defines commands in the 'config:*' namespace.
+ */
+class ConfigCommand extends BltTasks {
+
+  /**
+   * Gets the value of a config variables.
+   *
+   * @command config:get
+   *
+   * @param string $key The key for the configuration item to get.
+   *
+   * @throws \Acquia\Blt\Robo\Exceptions\BltException
+   */
+  public function getValue($key) {
+    if (!$this->getConfig()->has($key)) {
+      throw new BltException("$key is not set.");
+    }
+
+    $this->say($this->getConfigValue($key));
+  }
+
+  /**
+   * Dumps all config values.
+   *
+   * @command config:dump
+   */
+  public function dump() {
+    $config = $this->getConfig()->export();
+    $this->printArrayAsTable($config);
+  }
+
+}
