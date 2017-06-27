0b869cade905bc40da5a0771f890c06a48f082af
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-06-27 11:15:10 -0400

    Fixes #1719: Robo feature request: total "setup" time. (#1730)
    * Fixes #1719: Robo feature request: total "setup" time.
    
    * PHPCS.

--- bin/blt-robo-run.php
+++ bin/blt-robo-run.php
@@ -5,16 +5,23 @@
  * Execute BLT commands via Robo.
  */
 
-use Acquia\Blt\Robo\Config\YamlConfigProcessor;
-use Robo\Config\YamlConfigLoader;
-use Symfony\Component\Console\Input\ArgvInput;
 use Acquia\Blt\Robo\Blt;
 use Acquia\Blt\Robo\Config\DefaultConfig;
+use Acquia\Blt\Robo\Config\YamlConfigProcessor;
+use Robo\Common\TimeKeeper;
+use Robo\Config\YamlConfigLoader;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
+// Start Timer.
+$timer = new TimeKeeper();
+$timer->start();
+
+// Initialize input and output.
 $input = new ArgvInput($_SERVER['argv']);
 $output = new ConsoleOutput();
 
+// Initialize configuration.
 $config = new DefaultConfig($repo_root);
 $loader = new YamlConfigLoader();
 $processor = new YamlConfigProcessor();
@@ -25,6 +32,14 @@ $processor->extend($loader->load($config->get('repo.root') . '/blt/project.local
 $config->import($processor->export());
 $config->populateHelperConfig();
 
+// Execute command.
 $blt = new Blt($config, $input, $output);
 $status_code = (int) $blt->run($input, $output);
+
+// Stop timer.
+$timer->stop();
+if ($output->isVerbose()) {
+  $output->writeln("<comment>" . $timer->formatDuration($timer->elapsed()) . "</comment> total time elapsed.");
+}
+
 exit($status_code);
