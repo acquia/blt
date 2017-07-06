a706a8546034782a4813f06fcce707b0c6cd70d1
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-07-06 11:14:15 -0400

    Returning 0 from disabled commands. (#1759)

--- src/Robo/Application.php
+++ src/Robo/Application.php
@@ -3,6 +3,7 @@
 namespace Acquia\Blt\Robo;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application as ConsoleApplication;
@@ -15,15 +16,27 @@ use Symfony\Component\Console\Application as ConsoleApplication;
 class Application extends ConsoleApplication {
 
   /**
-   * @param \Symfony\Component\Console\Command\Command $command
-   * @param \Symfony\Component\Console\Input\InputInterface $input
-   * @param \Symfony\Component\Console\Output\OutputInterface $output
-   *
-   * @return int
-   *   The exit code.
+   * This command is identical to its parent, but public rather than protected.
    */
   public function runCommand(Command $command, InputInterface $input, OutputInterface $output) {
     return $this->doRunCommand($command, $input, $output);
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  protected function doRunCommand(Command $command, InputInterface $input, OutputInterface $output) {
+    $exit_code = parent::doRunCommand($command, $input, $output);
+
+    // If we disabled a command, do not consider it a failure. Because of this
+    // logic, we MUST throw exceptions in all cases of failure for BLT commands.
+    // Otherwise, we might disable a command, execute it in the VM via
+    // executeInDrupalVm(), and return a false success.
+    if ($exit_code == ConsoleCommandEvent::RETURN_CODE_DISABLED) {
+      $exit_code = 0;
+    }
+
+    return $exit_code;
   }
 
 }
