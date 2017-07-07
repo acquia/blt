531f6b125f4cb260276abfeb1ffe2455baf523fc
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-07-07 09:52:51 -0400

    Fixes #1771: blt vm writes to bashrc in DrupalVM and leaves it unwritable. (#1773)

--- scripts/drupal-vm/post-provision.sh
+++ scripts/drupal-vm/post-provision.sh
@@ -16,6 +16,9 @@ BLT_ALIAS_FILE="./vendor/acquia/blt/scripts/blt/alias"
 if [ -f "$BLT_ALIAS_FILE" ]
 then
     grep -q -F 'function blt' /home/vagrant/.bashrc || (cat "$BLT_ALIAS_FILE" /home/vagrant/.bashrc > temp && mv temp /home/vagrant/.bashrc)
+    chown vagrant /home/vagrant/.bashrc
+    chgrp vagrant /home/vagrant/.bashrc
+
 else
     echo "Make sure you're in the project root and have run composer install."
     exit 1
