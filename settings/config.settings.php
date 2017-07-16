a1fab103123cc3b11787a561ef7f4c5f96ae946d
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-07-16 12:33:07 -0400

    Allowing $split to be overridden. (#1804)

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -10,27 +10,32 @@ $split_envs = array('local', 'dev', 'stage', 'prod', 'ci', 'ah_other');
 foreach ($split_envs as $split_env) {
   $config["$split_filename_prefix.$split_env"]['status'] = FALSE;
 }
-$split = 'none';
-// Non-acquia envs.
-if ($is_local_env) {
-  $split = 'local';
-  if (getenv('TRAVIS') || getenv('PIPELINE_ENV') || getenv('PROBO_ENVIRONMENT')) {
-    $split = 'ci';
-  }
-}
-// Acquia only envs.
-elseif ($is_ah_env) {
-  $config_directories['vcs'] = $config_directories['sync'];
 
-  $split = 'ah_other';
-  if ($is_ah_dev_env || $is_ah_ode_env) {
-    $split = 'dev';
+// Do not set split unless it is unset. This allows prior scripts to set it.
+if (!isset($split)) {
+  $split = 'none';
+
+  // Non-acquia envs.
+  if ($is_local_env) {
+    $split = 'local';
+    if (getenv('TRAVIS') || getenv('PIPELINE_ENV') || getenv('PROBO_ENVIRONMENT')) {
+      $split = 'ci';
+    }
   }
-  elseif ($is_ah_stage_env) {
-    $split = 'stage';
-  }
-  elseif ($is_ah_prod_env) {
-    $split = 'prod';
+  // Acquia only envs.
+  elseif ($is_ah_env) {
+    $config_directories['vcs'] = $config_directories['sync'];
+
+    $split = 'ah_other';
+    if ($is_ah_dev_env || $is_ah_ode_env) {
+      $split = 'dev';
+    }
+    elseif ($is_ah_stage_env) {
+      $split = 'stage';
+    }
+    elseif ($is_ah_prod_env) {
+      $split = 'prod';
+    }
   }
 }
 
