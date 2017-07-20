1d0a4f1b6113d6cc7be1fa791e0fc43b1cb4cb18
Matthew Grasmick <matt.grasmick@gmail.com>
2017-07-20 12:25:16 -0400

    Fixing.

--- scripts/blt/ci/internal/run_tests.sh
+++ scripts/blt/ci/internal/run_tests.sh
@@ -7,8 +7,8 @@ yaml-cli update:value blt/project.yml project.local.hostname '127.0.0.1:8888'
 # Build codebase, validate, install Drupal, run basic tests.
 yaml-cli update:value blt/project.yml cm.strategy none
 blt validate:all
-blt setup --define environment=ci --yes
-blt tests:all --define tests.run-server=true --yes
+blt setup --define environment=ci --yes -vvv
+blt tests:all --define tests.run-server=true --yes -vvv
 blt tests:behat:definitions
 drush config-export --root=docroot -y
 
