31f6b563c3ed8facc5f00f98eaa6b09e9f954078
Matthew Grasmick <matt.grasmick@gmail.com>
2017-09-07 15:57:53 -0400

    Fixes #1543: Twig Lint is not maintained and fails valid twig code.

--- src/Robo/Commands/Validate/TwigCommand.php
+++ src/Robo/Commands/Validate/TwigCommand.php
@@ -26,35 +26,8 @@ class TwigCommand extends BltTasks {
     $fileset_ids = $this->getConfigValue('validate.twig.filesets');
     $filesets = $fileset_manager->getFilesets($fileset_ids);
     $bin = $this->getConfigValue('composer.bin');
-    $command = "'$bin/twig-lint' lint --only-print-errors '%s'";
+    $command = "'$bin/blt lint:twig:files '%s'";
     $this->executeCommandAgainstFilesets($filesets, $command, TRUE);
-  }
-
-  /**
-   * Executes Twig validator against a list of files, if in twig.filesets.
-   *
-   * @command validate:twig:files
-   *
-   * @param string $file_list
-   *   A list of files to scan, separated by \n.
-   */
-  public function lintFileList($file_list) {
-    $this->say("Linting twig files...");
-
-    $files = explode("\n", $file_list);
-
-    /** @var \Acquia\Blt\Robo\Filesets\FilesetManager $fileset_manager */
-    $fileset_manager = $this->getContainer()->get('filesetManager');
-    $fileset_ids = $this->getConfigValue('validate.twig.filesets');
-    $filesets = $fileset_manager->getFilesets($fileset_ids);
-
-    $bin = $this->getConfigValue('composer.bin');
-    $command = "'$bin/twig-lint' lint --only-print-errors '%s'";
-    foreach ($filesets as $fileset_id => $fileset) {
-      $filesets[$fileset_id] = $fileset_manager->filterFilesByFileset($files, $fileset);
-    }
-
-    $this->executeCommandAgainstFilesets($filesets, $command);
   }
 
 }
