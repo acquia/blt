d19257cc6bdd5dd732d2ad3848a4980ef09a9f66
Adam Malone <adam@adammalone.net>
2017-09-16 04:17:13 +1000

    Uses array_key_exists to check $SERVER for HTTPS key. (#1999)

--- settings/simplesamlphp.settings.php
+++ settings/simplesamlphp.settings.php
@@ -8,7 +8,7 @@ if (isset($_ENV['AH_SITE_NAME']) && is_dir('/var/www/html/' . $_ENV['AH_SITE_NAM
   // Force server port to 443 with HTTPS environments when behind a load balancer
   // which is a requirement for SimpleSAML with ADFS when providing a redirect path.
   // @see https://github.com/simplesamlphp/simplesamlphp/issues/450
-  if ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] === 'on') {
+  if (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS'] === 'on') {
     $_SERVER['SERVER_PORT'] = 443;
   }
 }
