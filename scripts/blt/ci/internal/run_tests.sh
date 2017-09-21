2e7ad211f1a47c25b6aa0e9166cb61ddbbfcf4ca
Matthew Grasmick <matt.grasmick@gmail.com>
2017-09-21 15:55:46 -0400

    docroot

--- scripts/blt/ci/internal/run_tests.sh
+++ scripts/blt/ci/internal/run_tests.sh
@@ -10,30 +10,40 @@ blt validate:all
 blt setup --define environment=ci --yes -vvv
 blt tests:all --define tests.run-server=true --yes -vvv
 blt tests:behat:definitions
-drush en config -y --root=docroot
-drush cc drush --root=docroot
-drush config-export --root=docroot -y
+cd docroot
+drush config-export -y
+cd ..
 
 # Test core-only config management.
 yaml-cli update:value blt/project.yml cm.strategy core-only
-drush config-export --root=docroot -y
+cd docroot
+drush config-export -y
+cd ..
 blt setup:config-import
 
 # Test features config management.
 yaml-cli update:value blt/project.yml cm.strategy features
 rm -rf config/default/*
-drush en features --root=docroot -y
-drush config-export --root=docroot -y
+cd docroot
+drush en features -y
+drush config-export -y
+cd ..
 blt setup:config-import
-drush pm-uninstall features --root=docroot -y
+cd docroot
+drush pm-uninstall features -y
+cd ..
 
 # Test config split.
 yaml-cli update:value blt/project.yml cm.strategy config-split
-drush en config_split --root=docroot -y
-drush config-export --root=docroot -y
+cd docroot
+drush en config_split -y
+drush config-export -y
+cd ..
 mv ${BLT_DIR}/scripts/blt/ci/internal/config_split.config_split.ci.yml config/default/
 blt setup:config-import
-drush pm-uninstall config_split --root=docroot -y
+cd docroot
+drush pm-uninstall config_split -y
+cd ..
 
 # Remove all exported configuration from previous test steps.
 rm -rf config/default/*
