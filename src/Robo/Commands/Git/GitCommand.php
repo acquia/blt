ef30f559be4aa58de9c7fdf346ca9b2e90e2900e
Dane Powell <git@danepowell.com>
2017-10-03 15:15:01 -0700

    Refactor git:pre-commit to use collection builder.

--- src/Robo/Commands/Git/GitCommand.php
+++ src/Robo/Commands/Git/GitCommand.php
@@ -44,25 +44,41 @@ class GitCommand extends BltTasks {
    * @param string $changed_files
    *   A list of staged files, separated by \n.
    *
-   * @return int
+   * @return \Robo\Result
    */
   public function preCommitHook($changed_files) {
-    $this->invokeCommands([
-      // Passing a file list to be PHPCS will cause all specified files to
-      // be sniffed, regardless of the extensions or patterns defined in
-      // phpcs.xml. So, we do not use validate:phpcs:files.
-      'validate:phpcs' => [],
-      'validate:twig:files' => ['file_list' => $changed_files],
-      'validate:yaml:files' => ['file_list' => $changed_files],
-    ]);
+    $collection = $this->collectionBuilder();
+    $collection->setProgressIndicator(NULL);
+    $collection->addCode(
+      function () use ($changed_files) {
+        return $this->invokeCommands([
+          // Passing a file list to be PHPCS will cause all specified files to
+          // be sniffed, regardless of the extensions or patterns defined in
+          // phpcs.xml. So, we do not use validate:phpcs:files.
+          'validate:phpcs' => [],
+          'validate:twig:files' => ['file_list' => $changed_files],
+          'validate:yaml:files' => ['file_list' => $changed_files],
+        ]);
+      }
+    );
 
     $changed_files_list = explode("\n", $changed_files);
-    if (in_array(['composer.json', 'composer.lock'], $changed_files_list)) {
-      $this->invokeCommand('validate:composer', ['file_list' => $changed_files]);
+    if (in_array('composer.json', $changed_files_list)
+      || in_array('composer.lock', $changed_files_list)) {
+      $collection->addCode(
+        function () use ($changed_files) {
+          return $this->invokeCommand('validate:composer');
+        }
+      );
     }
 
-    $this->invokeHook('pre-commit');
-    $this->say("<info>Your local code has passed git pre-commit validation.</info>");
+    $result = $collection->run();
+
+    if ($result->wasSuccessful()) {
+      $this->say("<info>Your local code has passed git pre-commit validation.</info>");
+    }
+
+    return $result;
   }
 
 }
