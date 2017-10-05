651f97cbc87bb15e606e7200c85f17e5c236b1fb
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-10-05 14:10:14 -0400

    Temporarily deprecating commands broken by drush 9. (#2089)
    * Temporarily deprecating commands broken by drush 9.
    
    * Swapping drush --no with --no-interaction.
    
    * Fixing drush status check.
    
    * Changing dirs.
    
    * Forcing min version drush 9.0.0-beta5.
    
    * Fixing test.

--- src/Robo/Commands/Blt/DoctorCommand.php
+++ src/Robo/Commands/Blt/DoctorCommand.php
@@ -19,6 +19,10 @@ class DoctorCommand extends BltTasks {
    */
   public function doctor() {
 
+    $this->logger->error("This command has been temporarily deprecated due to breaking changes in Drush 9.");
+    return 1;
+
+    // @codingStandardsIgnoreStart
     $this->taskDrush()
       ->drush('cc drush')
       ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
@@ -56,6 +60,7 @@ class DoctorCommand extends BltTasks {
     }
 
     return $result->getMessage();
+    // @codingStandardsIgnoreEnd
   }
 
   /**
