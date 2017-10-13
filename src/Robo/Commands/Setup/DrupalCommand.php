812de25024d8f3fb8bf6313b1ad171c566c511b8
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-10-13 16:11:42 -0400

    Fixes #2135: Silent Failure During Config-Import. (#2137)

--- src/Robo/Commands/Setup/DrupalCommand.php
+++ src/Robo/Commands/Setup/DrupalCommand.php
@@ -4,6 +4,7 @@ namespace Acquia\Blt\Robo\Commands\Setup;
 
 use Acquia\Blt\Robo\BltTasks;
 use Acquia\Blt\Robo\Common\RandomString;
+use Acquia\Blt\Robo\Exceptions\BltException;
 
 /**
  * Defines commands in the "drupal:*" namespace.
@@ -57,6 +58,9 @@ class DrupalCommand extends BltTasks {
     $result = $task->detectInteractive()->run();
     if ($result->wasSuccessful()) {
       $this->getConfig()->set('state.drupal.installed', TRUE);
+    }
+    else {
+      throw new BltException("Failed to install Drupal!");
     }
 
     return $result;
