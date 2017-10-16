b19c3d8e52ede749362454fac9b5f112eb5b56bd
Dane Powell <danepowell@users.noreply.github.com>
2017-10-16 16:21:48 -0400

    Updating version to 8.9.9

--- src/Robo/Blt.php
+++ src/Robo/Blt.php
@@ -37,7 +37,7 @@ class Blt implements ContainerAwareInterface, LoggerAwareInterface {
   /**
    * The BLT version.
    */
-  const VERSION = '8.9.8';
+  const VERSION = '8.9.9';
 
   /**
    * The Robo task runner.
