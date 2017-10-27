c2d1d50c2b751889d255f6f85d194d9df9896e96
Dane Powell <git@danepowell.com>
2017-10-27 16:09:22 -0700

    Removed boilerplate.

--- drush/Commands/BltCommands.php
+++ drush/Commands/BltCommands.php
@@ -3,15 +3,6 @@ namespace Drush\Commands\Blt;
 
 use Drush\Commands\DrushCommands;
 
-/**
- *
- * In addition to a commandfile like this one, you need a drush.services.yml
- * in root of your module.
- *
- * See these files for an example of injecting Drupal services:
- *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
- *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
- */
 class BltCommands extends DrushCommands {
 
   /**
