e05923e4ebcb38532f0ef50e1d28021fb1838c10
Lindsey Catlett <lindsey.catlett@acquia.com>
2017-10-31 15:19:44 -0400

    Clean up to pass phpcs tests.

--- settings/acsf/post-install/post-install.php
+++ settings/acsf/post-install/post-install.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * Factory Hook: post-install
+ * @file
+ * Factory Hook: post-install.
  *
  * This hook enables you to execute PHP after a new website is created
  * in your subscription. Unlike most API-based hooks, this hook does not
  * take arguments, but instead executes the PHP code it is provided.
- *
  */
 
-$site=$_ENV['AH_SITE_GROUP'];
-$env=$_ENV['AH_SITE_ENVIRONMENT'];
+$site = $_ENV['AH_SITE_GROUP'];
+$env = $_ENV['AH_SITE_ENVIRONMENT'];
 
-// The public domain name of the website. 
-// HTTP host is used to run updates against the requested domain rather than the acsf primary domain. 
-$domain=$_SERVER['HTTP_HOST'];
+// The public domain name of the website.
+// Run updates against requested domain rather than acsf primary domain.
+$domain = $_SERVER['HTTP_HOST'];
 
 
 exec('/mnt/www/html/$target_env/vendor/acquia/blt/bin/blt deploy:update --define environment=$env --define drush.uri=$domain -v -y');
