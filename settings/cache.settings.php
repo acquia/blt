abaa127f0229cbb88cffc644829015d19b17a6c9
Lindsey Catlett <lindsey.catlett@acquia.com>
2017-11-30 13:01:49 -0500

    Add example memcache and config override settings.

--- settings/cache.settings.php
+++ settings/cache.settings.php
@@ -8,3 +8,21 @@
 if ($is_prod_env || $is_stage_env) {
   $config['system.logging']['error_level'] = 'hide';
 }
+
+
+/**
+ * Override static cache defaults in core.services.yml
+ * 
+ * Override the default backend set on the service definition on a per-bin basis. 
+ * 
+ * The bootstrap, discovery and config bins use the chainedfast backend by default and
+ * since the core service definition for these bins sets the expire to 
+ * CacheBackendInterface::CACHE_PERMANENT, these objects are cached permanetly by default
+ * and may result in stale configuration whjen rebuilding the service container on deploymemnts 
+ * and cache rebuilds. Uncomment the relevant bins below to change this behavior. 
+ * See https://www.drupal.org/node/2754947
+ */
+
+ // $settings['cache']['bins']['bootstrap'] = 'cache.backend.null';
+ // $settings['cache']['bins']['discovery'] = 'cache.backend.null';
+ // $settings['cache']['bins']['config'] = 'cache.backend.null';
