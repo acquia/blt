745cf8153272fa9bf062eaeb3a345a2169edcf7a
Lindsey Catlett <lindsey.catlett@acquia.com>
2017-11-30 22:23:38 -0500

    Update docs and phpcs cleanup.

--- settings/cache.settings.php
+++ settings/cache.settings.php
@@ -11,18 +11,17 @@ if ($is_prod_env || $is_stage_env) {
 
 
 /**
- * Override static cache defaults in core.services.yml
- * 
- * Override the default backend set on the service definition on a per-bin basis. 
- * 
- * The bootstrap, discovery and config bins use the chainedfast backend by default and
- * since the core service definition for these bins sets the expire to 
- * CacheBackendInterface::CACHE_PERMANENT, these objects are cached permanetly by default
- * and may result in stale configuration whjen rebuilding the service container on deploymemnts 
- * and cache rebuilds. Uncomment the relevant bins below to change this behavior. 
- * See https://www.drupal.org/node/2754947
+ * Example: Override required core defaults to use alternative cache backends.
+ *
+ * The bootstrap, discovery, and config bins use the chainedfast backend by
+ * default and since the core service definition for these bins sets
+ * the expire to CacheBackendInterface::CACHE_PERMANENT, these objects
+ * are cached permanently and may result in stale configuration when
+ * rebuilding the service container on deploymemnts and cache rebuilds.
+ * Uncomment the relevant lines for each bin below and configure the desired
+ * See https://www.drupal.org/node/2754947.
  */
 
- // $settings['cache']['bins']['bootstrap'] = 'cache.backend.null';
- // $settings['cache']['bins']['discovery'] = 'cache.backend.null';
- // $settings['cache']['bins']['config'] = 'cache.backend.null';
+// $settings['cache']['bins']['bootstrap'] = 'cache.backend.null';
+// $settings['cache']['bins']['discovery'] = 'cache.backend.null';
+// $settings['cache']['bins']['config'] = 'cache.backend.null';.
