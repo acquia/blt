e0a6500d42bf013d022885dbbf7a60c0f7fb8da1
Matthew Grasmick <matthew.grasmick@acquia.com>
2017-12-13 10:47:45 -0500

    Fixes #2277: Add internal Travis CI build to test drupal/core HEAD. (#2368)
    * Fixes #2277: Add internal Travis CI build to test drupal/core HEAD.
    
    * Fixing composer require.

--- scripts/blt/ci/internal/create_blt_project.sh
+++ scripts/blt/ci/internal/create_blt_project.sh
@@ -10,6 +10,11 @@ export PATH=${COMPOSER_BIN}:${PATH}
 
 ${BLT_DIR}/vendor/bin/robo create:from-symlink --no-vm
 
+if [ "${DRUPAL_CORE_VERSION}" != "default" ]; then
+  composer require "drupal/core:${DRUPAL_CORE_VERSION}" --no-update --no-interaction
+  composer update --no-interaction
+fi
+
 export PATH=${BLT_DIR}/../blted8/vendor/bin:$PATH
 
 # Define BLT's deployment endpoints.
