142fd5a7e3489de8d26918cdd0eb2c7ae08e56a3
Matthew Grasmick <matt.grasmick@gmail.com>
2017-12-19 14:34:42 -0500

    Fix MySQL access.

--- src/Robo/Commands/Vm/LandoCommand.php
+++ src/Robo/Commands/Vm/LandoCommand.php
@@ -3,6 +3,7 @@
 namespace Acquia\Blt\Robo\Commands\Vm;
 
 use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Common\YamlMunge;
 use Acquia\Blt\Robo\Exceptions\BltException;
 
 /**
@@ -22,12 +23,27 @@ class LandoCommand extends BltTasks {
       throw new BltException("lando must be installed on this machine.");
     }
 
+    // Change default drupal.db.host value to "database".
+    $project_yml = YamlMunge::parseFile($this->getConfigValue('blt.config-files.project'));
+    $project_yml['drupal']['db']['host'] = 'database';
+    YamlMunge::writeFile($this->getConfigValue('blt.config-files.project'), $project_yml);
+
+    // Re-generate local.settings.php to use new drupal.db.host value.
+    $site = $this->getConfigValue('site');
+    $local_settings_file = "$site/settings/local.settings.php";
+    $this->taskFilesystemStack()->remove($local_settings_file);
+    $this->invokeCommand('setup:settings');
+
+    // Copy default .lando.yml file into place.
     $this->taskFilesystemStack()
-      ->copy($this->getConfigValue('blt.root') . '/scripts/lando/.lando.yml',
+      ->copy(
+        $this->getConfigValue('blt.root') . '/scripts/lando/.lando.yml',
         $this->getConfigValue('repo.root') . '/.lando.yml'
-        )
+      )
       ->run();
-    $this->getConfig()->expandFileProperties($this->getConfigValue('repo.root') . '/.lando.yml');
+    $this->getConfig()->expandFileProperties($this->getConfigValue('repo.root') . '/.lando.yml');[]
+
+    // Make "blt" bin accessible to $PATH in Lando container.
     $this->taskExecStack()
       ->dir($this->getConfigValue('repo.root'))
       ->exec('lando ssh -u root -c "ln -s /app/vendor/bin/blt /usr/bin/blt"')
