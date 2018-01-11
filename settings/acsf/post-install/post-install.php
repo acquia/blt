9afb53227cd7777ddd6cd412f8f47ab327e3173c
Dan Montgomery <damontgomery@gmail.com>
2018-01-11 12:03:43 -0600

    ACSF Factory Hooks (#2440)
    * Clear twig caches on all acsf webs on theme deploy.
    
    * Install sites via blt on acsf site install.
    
    * Use drush vendor local rather than global to support core 8.4.
    
    * Clean up to pass phpcs tests.
    
    * Update post-install.php
    
    * Update clear-twig-cache.sh
    
    * Update post-install.php
    
    * Update post-install.php
    
    * PHPCS fixes.
    
    * Used full option naming.
    
    * Update comments to clarify ACSF hooks.

---
+++ settings/acsf/post-install/post-install.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Factory Hook: post-install.
+ *
+ * This hook enables you to execute PHP after a new website is created
+ * in your subscription. Unlike most API-based hooks, this hook does not
+ * take arguments, but instead executes the PHP code it is provided.
+ *
+ * This is used so that an ACSF site install will match a local BLT site
+ * install. After a local site install, the update functions are run.
+ *
+ */
+
+$site = $_ENV['AH_SITE_GROUP'];
+$env = $_ENV['AH_SITE_ENVIRONMENT'];
+
+// The public domain name of the website.
+// Run updates against requested domain rather than acsf primary domain.
+$domain = $_SERVER['HTTP_HOST'];
+
+exec("/mnt/www/html/$target_env/vendor/acquia/blt/bin/blt deploy:update --define environment=$env --define drush.uri=$domain --verbose --yes");
