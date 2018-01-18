e1efa4250a06ffd8079b810af2ba72f5bda90957
Matthew Grasmick <matt.grasmick@gmail.com>
2018-01-18 16:07:54 -0500

    kill config.

--- tests/phpunit/BltProject/LightningTest.php
+++ tests/phpunit/BltProject/LightningTest.php
@@ -10,11 +10,13 @@ use Acquia\Blt\Tests\BltProjectTestBase;
 class LightningTest extends BltProjectTestBase {
 
   public function testSetup() {
+    // @todo Determine why this is necessary on Travis but not locally.
+    $this->fs->remove($this->sandboxInstance . "/config/default");
+    $this->fs->mkdir($this->sandboxInstance . "/config/default");
     $this->blt("setup", [
       '--define' => [
         'project.profile.name=lightning',
       ],
-      '--yes' => TRUE,
     ]);
   }
 
