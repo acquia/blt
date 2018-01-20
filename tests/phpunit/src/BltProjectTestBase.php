0a7ade90f8d62b31bbe0baea43cc2e09b85d06a3
Matthew Grasmick <matthew.grasmick@acquia.com>
2018-01-19 21:11:45 -0500

    Adding more tests. (#2462)
    * Adding more tests.
    
    * unset container.
    
    * returning config.

--- tests/phpunit/src/BltProjectTestBase.php
+++ tests/phpunit/src/BltProjectTestBase.php
@@ -5,6 +5,7 @@ namespace Acquia\Blt\Tests;
 use Acquia\Blt\Robo\Blt;
 use Acquia\Blt\Robo\Config\ConfigInitializer;
 use Acquia\Blt\Robo\Commands\Input\ArrayInput;
+use Robo\Robo;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Terminal;
@@ -237,11 +238,11 @@ abstract class BltProjectTestBase extends \PHPUnit_Framework_TestCase {
       $output = new BufferedOutput();
     }
 
-    // Re-initialize config.
-    $this->reInitializeConfig($input);
+    $config_initializer = new ConfigInitializer($this->sandboxInstance, $input);
+    $config = $config_initializer->initialize();
 
     // Execute command.
-    $blt = new Blt($this->config, $input, $output);
+    $blt = new Blt($config, $input, $output);
     $status_code = (int) $blt->run($input, $output);
 
     if (getenv('BLT_PRINT_COMMAND_OUTPUT')) {
@@ -253,7 +254,7 @@ abstract class BltProjectTestBase extends \PHPUnit_Framework_TestCase {
       throw new \Exception("BLT command exited with non-zero exit code.");
     }
 
-    return [$status_code, $output->fetch()];
+    return [$status_code, $output->fetch(), $config];
   }
 
   /**
@@ -292,6 +293,8 @@ abstract class BltProjectTestBase extends \PHPUnit_Framework_TestCase {
 
   protected function tearDown() {
     $this->dropDatabase();
+    unset($this->config);
+    Robo::unsetContainer();
   }
 
   protected function printTestName() {
