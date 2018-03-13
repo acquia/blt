06f712acc52bc13620e0e6b2fa8567b23d6337c6
Matthew Grasmick <matthew.grasmick@acquia.com>
2018-03-13 15:27:58 -0400

    Revert "Fixes #2628: Correctly pass verbosity to Drush." (#2634)
    * Revert "Update README.md"
    
    This reverts commit 525ba67f161aa6bf5292e221043ddcdf48ccd815.
    
    * Revert "Fixes #2628: Correctly pass verbosity to Drush. (#2631)"
    
    This reverts commit d5059bd0436a1ec5ae1a480eb60e8fbca3a94bad.

--- src/Robo/Tasks/LoadTasks.php
+++ src/Robo/Tasks/LoadTasks.php
@@ -11,12 +11,8 @@ trait LoadTasks {
    * @return \Acquia\Blt\Robo\Tasks\DrushTask
    */
   protected function taskDrush() {
-    /** @var \Acquia\Blt\Robo\Tasks\DrushTask $task */
     $task = $this->task(DrushTask::class);
     $task->setInput($this->input());
-    /** @var \Symfony\Component\Console\Output\OutputInterface $output */
-    $output = $this->output();
-    $task->setVerbosityThreshold($output->getVerbosity());
 
     return $task;
   }
