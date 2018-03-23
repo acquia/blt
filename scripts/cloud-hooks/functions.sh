8990a9f160bf2ee4cb6af30bbe47b3a389ad2017
Lindsey Catlett <lindsey.catlett@gmail.com>
2018-03-23 17:20:05 -0400

    Backport Hooks to exclude prod and update environments in Cloud Hook scripts.

--- scripts/cloud-hooks/functions.sh
+++ scripts/cloud-hooks/functions.sh
@@ -8,10 +8,14 @@ drush_alias=${site}'.'${target_env}
 deploy_updates() {
 
   case $target_env in
-    01dev|01test|01live)
+    01dev|01test)
       acsf_deploy
       ;;
-    01devup|01testup|01update)
+    # Do not run deploy updates on 01live in case a branch is deployed in prod. 
+    01devup|01testup|01update|01live)
+      ;;
+    ode[[:digit:]]*)
+      deploy_install
       ;;
     *)
       ace_deploy
@@ -69,6 +73,24 @@ ace_deploy() {
   fi
 
   echo "Finished updates for environment: $target_env"
+}
+
+deploy_sync() {
+
+  echo "Running sync refresh for environment: $target_env"
+
+  # Prep for BLT commands.
+  repo_root="/var/www/html/$site.$target_env"
+  export PATH=$repo_root/vendor/bin:$PATH
+  cd $repo_root
+
+  blt deploy:sync:refresh --define environment=$target_env -v -y
+  if [ $? -ne 0 ]; then
+      echo "Sync errored."
+      exit 1
+  fi
+
+  echo "Finished sync for environment: $target_env"
 }
 
 deploy_install() {
