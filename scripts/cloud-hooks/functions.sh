2833fdaf7bf62ccef782c15e1f2e941449caf2d3
Amit Vyas <vyasamit2007@gmail.com>
2018-04-02 23:57:10 +0530

    ACSF Cloud Hooks / Factory Hooks Enhancements (#2685)

--- scripts/cloud-hooks/functions.sh
+++ scripts/cloud-hooks/functions.sh
@@ -7,16 +7,14 @@ drush_alias=${site}'.'${target_env}
 
 deploy_updates() {
 
-  case $target_env in
-    01dev|01test|01live)
-      acsf_deploy
-      ;;
-    01devup|01testup|01update)
-      ;;
-    *)
-      ace_deploy
-      ;;
-    esac
+  if [[ $target_env =~ [0-9][1-9](dev|test) ]]; then
+    acsf_deploy
+  elif [[ $target_env =~ [0-9][1-9](devup|testup|update)|[0-9][1-9](live)|ode[0-9]* ]]; then
+    deploy_install
+  else
+    ace_deploy
+  fi
+
 }
 
 acsf_deploy() {
@@ -69,6 +67,24 @@ ace_deploy() {
   fi
 
   echo "Finished updates for environment: $target_env"
+}
+
+deploy_sync() {
+
+  echo "Running sync refresh for environment: $target_env"
+
+  # Prep for BLT commands.
+  repo_root="/var/www/html/$site.$target_env"
+  export PATH=$repo_root/vendor/bin:$PATH
+  cd $repo_root
+
+  blt deploy:sync:refresh --define environment=$target_env -v -y
+  if [ $? -ne 0 ]; then
+      echo "Sync errored."
+      exit 1
+  fi
+
+  echo "Finished sync for environment: $target_env"
 }
 
 deploy_install() {
---
+++ settings/acsf/db-update/db-update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Factory Hook: db-update
+#
+# The existence of one or more executable files in the
+# /factory-hooks/db-update directory will prompt them to be run *instead of* the
+# regular database update (drush updatedb) command. So that update command will
+# normally be part of the commands executed below.
+#
+# Usage: post-code-deploy site env db-role domain custom-arg
+# Map the script inputs to convenient names.
+# Acquia hosting site / environment names
+site="$1"
+env="$2"
+# database role. (Not expected to be needed in most hook scripts.)
+db_role="$3"
+# The public domain name of the website.
+domain="$4"
+
+# BLT executable:
+blt="/var/www/html/$site.$env/vendor/acquia/blt/bin/blt"
+
+deployupdate="$blt artifact:update:drupal:all-sites --define environment=$env --define drush.uri=$domain --verbose --yes"
+
+$deployupdate
