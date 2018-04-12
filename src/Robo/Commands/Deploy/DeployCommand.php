ff81245f6d0caacca199d6343393146641d24498
Matthew Grasmick <matthew.grasmick@acquia.com>
2018-04-12 15:10:43 -0500

    Update DeployCommand.php

--- src/Robo/Commands/Deploy/DeployCommand.php
+++ src/Robo/Commands/Deploy/DeployCommand.php
@@ -412,8 +412,6 @@ class DeployCommand extends BltTasks {
       $filepath = $this->getInspector()->getFs()->makePathRelative($deployment_identifier_file, $this->getConfigValue('repo.root'));
       throw new BltException("Unable to write deployment identifier to $filepath.");
     }
-
-    return $result->getExitCode();
   }
 
   /**
