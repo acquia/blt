9ba09370bfa308b9ed6d7f6d7cd83d3c43754dce
Mike Madison <mike.madison@acquia.com>
2018-04-18 09:54:38 -0700

    Fixes #2742 to include a composer munge as part of the 9.1 update process. (#2745)

--- src/Update/Updates.php
+++ src/Update/Updates.php
@@ -559,6 +559,24 @@ class Updates {
     $this->updater->getOutput()->writeln("");
     $this->updater->getOutput()->writeln($formattedBlock);
     $this->updater->getOutput()->writeln("");
+
+    // Update composer.json to include new BLT required/suggested files.
+    // Pulls in wikimedia/composer-merge-plugin and composer/installers settings.
+    $project_composer_json = $this->updater->getRepoRoot() . '/composer.json';
+    $template_composer_json = $this->updater->getBltRoot() . '/template/composer.json';
+    $munged_json = ComposerMunge::mungeFiles($project_composer_json, $template_composer_json);
+    $bytes = file_put_contents($project_composer_json, $munged_json);
+    if (!$bytes) {
+      $messages = ["Could not update $project_composer_json."];
+    }
+    else {
+      $messages = ["Updated $project_composer_json. Review changes, then re-run composer update."];
+    }
+
+    $formattedBlock = $this->updater->getFormatter()->formatBlock($messages, 'ice');
+    $this->updater->getOutput()->writeln("");
+    $this->updater->getOutput()->writeln($formattedBlock);
+    $this->updater->getOutput()->writeln("");
   }
 
 }
