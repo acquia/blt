a89a26a270dee64c9b57838c4fa14ca15e6eb770
Lindsey Catlett <lindsey.catlett@gmail.com>
2018-06-01 04:53:30 -0400

    Use global acsf_site_name variable to set active site splits.

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -95,7 +95,8 @@ if ($split != 'none') {
  */
 $config["$split_filename_prefix.$site_dir"]['status'] = TRUE;
 
-// Set acsf site split.
+// Set acsf site split if explicit global exists.
+global $acsf_site_name;
 if (isset($acsf_site_name)) {
   $config["$split_filename_prefix.$acsf_site_name"]['status'] = TRUE;
 }
