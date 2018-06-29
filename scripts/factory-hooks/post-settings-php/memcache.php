a5f7c7d80d3aec91d916384763d9a1612875afb4
Lindsey Catlett <lindsey.catlett@gmail.com>
2018-06-28 22:40:09 -0400

    Add post settings php hook to use BLT memcache settings and ACSF settings flag.

---
+++ scripts/factory-hooks/post-settings-php/memcache.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Example implementation of ACSF post-settings-php hook.
+ *
+ * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
+ */
+
+// Use ACSF feature flag to indicate if a site should use memcache backend
+if ($is_ah_env &&
+  !empty($site_settings['flags']['memcache_enabled'] &&
+  array_key_exists('memcache', $settings) &&
+  array_key_exists('servers', $settings['memcache']) &&
+  !empty($settings['memcache']['servers'])
+) {
+  require DRUPAL_ROOT . '/../vendor/acquia/blt/settings/memcache.settings.php';
+}
+
+
