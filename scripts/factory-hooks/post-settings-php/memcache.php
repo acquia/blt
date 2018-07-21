1c899d08b0e39e807b6d773a8329aee014258f3c
Lindsey Catlett <lindsey.catlett@gmail.com>
2018-07-21 11:05:17 -0400

    Update conditional to remote BLT variable dependency.
    Drupal may not be bootstrapped fully so global variables may not be set yet. Since memcache is being used as a backend for the service container cache during site installs, assuming that the BLT variable is set may cause issues.

--- scripts/factory-hooks/post-settings-php/memcache.php
+++ scripts/factory-hooks/post-settings-php/memcache.php
@@ -7,12 +7,10 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
-// Use ACSF feature flag to indicate if a site should use memcache backend
-if ($is_ah_env &&
-  !empty($site_settings['flags']['memcache_enabled']) &&
-  array_key_exists('memcache', $settings) &&
-  array_key_exists('servers', $settings['memcache']) &&
-  !empty($settings['memcache']['servers'])
+// Use ACSF internal settings site flag to apply memcache settings.
+if (getenv('AH_SITE_ENVIRONMENT') && 
+	isset($site_settings['flags']['memcache_enabled']) &&
+	isset($settings['memcache']['servers'])
 ) {
   require DRUPAL_ROOT . '/../vendor/acquia/blt/settings/memcache.settings.php';
 }
