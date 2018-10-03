a415bdc041e0e52bac21e2be4581f1fdee90e10d
Matthew Grasmick <matt.grasmick@gmail.com>
2018-10-03 09:45:58 -0400

    Undo namespace changes.

--- src/Robo/Hooks/WebServerHook.php
+++ src/Robo/Hooks/WebServerHook.php
@@ -3,23 +3,12 @@
 namespace Acquia\Blt\Robo\Hooks;
 
 use Acquia\Blt\Robo\BltTasks;
-use Acquia\Blt\Robo\Common\IO;
-use Acquia\Blt\Robo\Config\ConfigAwareTrait;
-use Acquia\Blt\Robo\Inspector\InspectorAwareTrait;
 use Consolidation\AnnotatedCommand\CommandData;
-use League\Container\ContainerAwareTrait;
-use Psr\Log\LoggerAwareTrait;
 
 /**
  * Starts and kills Drush webserver for @launchWebServer annotation.
  */
 class WebServerHook extends BltTasks {
-
-  use ConfigAwareTrait;
-  use ContainerAwareTrait;
-  use LoggerAwareTrait;
-  use InspectorAwareTrait;
-  use IO;
 
   protected $serverUrl;
   protected $serverPort;
