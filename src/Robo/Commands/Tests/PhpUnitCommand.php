c9a047d7c039369ce873d9ddc7b81d243398cb87
Dane Powell <danepowell@users.noreply.github.com>
2018-10-05 17:49:47 -0700

    Port PHPUnit fixes to 9.x (#3117)
    * Allow PHPUnit to bootstrap from core. (#3071)
    
    * Use core's PHPUnit bootstrap file. (#3092)
    
    * Remove support for PHPUnit 5. (#3102)

--- src/Robo/Commands/Tests/PhpUnitCommand.php
+++ src/Robo/Commands/Tests/PhpUnitCommand.php
@@ -19,12 +19,6 @@ class PhpUnitCommand extends BltTasks {
   protected $reportsDir;
 
   /**
-   * The bootstrap filename.
-   *
-   * @var string*/
-  protected $bootstrapFile;
-
-  /**
    * The filename for PHPUnit report.
    *
    * @var string*/
@@ -47,7 +41,6 @@ class PhpUnitCommand extends BltTasks {
     $this->reportFile = $this->reportsDir . '/results.xml';
     $this->testsDir = $this->getConfigValue('repo.root') . '/tests/phpunit';
     $this->phpunitConfig = $this->getConfigValue('phpunit');
-    $this->bootstrapFile = $this->getConfigValue('blt.root') . '/scripts/phpunit/bootstrap.php';
   }
 
   /**
@@ -60,7 +53,6 @@ class PhpUnitCommand extends BltTasks {
     $this->createLogs();
     foreach ($this->phpunitConfig as $test) {
       $task = $this->taskPHPUnit()
-        ->bootstrap($this->bootstrapFile)
         ->xml($this->reportFile)
         ->printOutput(TRUE)
         ->printMetadata(FALSE);
