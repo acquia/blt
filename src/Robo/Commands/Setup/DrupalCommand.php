9b55af46eea1d388c1470956e15e58416e1faf2b
Dane Powell <git@danepowell.com>
2018-11-16 13:43:34 -0800

    Add install_from_config variable.

--- src/Robo/Commands/Setup/DrupalCommand.php
+++ src/Robo/Commands/Setup/DrupalCommand.php
@@ -53,7 +53,8 @@ class DrupalCommand extends BltTasks {
     // Install site from existing config if supported.
     $strategy = $this->getConfigValue('cm.strategy');
     $cm_core_key = $this->getConfigValue('cm.core.key');
-    if (in_array($strategy, ['core-only', 'config-split']) && $cm_core_key == 'sync') {
+    $install_from_config = $this->getConfigValue('cm.core.install_from_config');
+    if (in_array($strategy, ['core-only', 'config-split']) && $cm_core_key == 'sync' && $install_from_config) {
       $core_config_file = $this->getConfigValue('docroot') . '/' . $this->getConfigValue("cm.core.dirs.$cm_core_key.path") . '/core.extension.yml';
       if (file_exists($core_config_file)) {
         $task->option('existing-config');
