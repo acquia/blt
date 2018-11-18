3d7f71250b8ff9c85895ab66e1a917dc4d998ff8
Grant Gaudet <grant@greylabel.net>
2018-11-17 17:30:56 -0800

    Update tests.

--- tests/phpunit/Blt/TestPhpUnitCommandTest.php
+++ tests/phpunit/Blt/TestPhpUnitCommandTest.php
@@ -10,22 +10,37 @@ use Acquia\Blt\Tests\BltProjectTestBase;
 class TestPhpUnitCommandTest extends BltProjectTestBase {
 
   /**
-   * Test that ExampleTest.php is correctly executed and passes.
-   */
-  public function testPhpUnitCommandExampleTests() {
-    list($status_code, $output, $config) = $this->blt("tests:phpunit:run");
-    $results = file_get_contents($this->sandboxInstance . '/reports/phpunit/results.xml');
-    $this->assertContains('tests="1" assertions="1"', $results);
-    $this->assertContains('name="testExample" class="My\Example\Project\Tests\ExampleTest"', $results);
-  }
-
-  /**
    * Tests that removing ExampleTest.php doesn't cause failure for users.
    */
   public function testPhpUnitCommandNoTests() {
     $this->fs->remove($this->sandboxInstance . "/tests/phpunit/ExampleTest.php");
-    list($status_code, $output, $config) = $this->blt("tests:phpunit:run");
+    $repoRroot = $this->config->get("repo.root");
+    list($status_code, $output, $config) = $this->blt("tests:phpunit:run", [
+      "--define" => [
+        "tests.phpunit.0.config=$repoRroot/tests/phpunit/phpunit.xml",
+        "tests.phpunit.0.path=$repoRroot/tests/phpunit",
+        "tests.phpunit.0.class=NULL",
+      ],
+    ]);
     $this->assertEquals(0, $status_code);
+  }
+
+  /**
+   * Test that ExampleTest.php is correctly executed and passes.
+   */
+  public function testPhpUnitCommandExampleTests() {
+    $docroot = $this->config->get("docroot");
+    $repoRroot = $this->config->get("repo.root");
+    list($status_code, $output, $config) = $this->blt("tests:phpunit:run", [
+      "--define" => [
+        "tests.phpunit.0.config=$docroot/core/phpunit.xml.dist",
+        "tests.phpunit.0.path=$repoRroot/tests/phpunit",
+        "tests.phpunit.0.class=$repoRroot/tests/phpunit/ExampleTest",
+      ],
+    ]);
+    $results = file_get_contents($this->sandboxInstance . '/reports/phpunit/results.xml');
+    $this->assertContains('tests="1" assertions="1"', $results);
+    $this->assertContains('name="testExample" class="My\Example\Project\Tests\ExampleTest"', $results);
   }
 
 }
