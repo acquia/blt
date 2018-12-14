909204751af6b0959d11527f78c8cb322a15d026
Matthew Grasmick <matt.grasmick@gmail.com>
2018-12-13 21:25:30 -0500

    Update db-update.sh
    Forward porting changes from 9.2.x.

--- scripts/factory-hooks/db-update/db-update.sh
+++ scripts/factory-hooks/db-update/db-update.sh
@@ -18,21 +18,27 @@ db_role="$3"
 domain="$4"
 
 # BLT executable:
-blt="/var/www/html/$site.$env/vendor/acquia/blt/bin/blt"
+blt="/mnt/www/html/$site.$env/vendor/acquia/blt/bin/blt"
 
 # You need the URI of the site factory website in order for drush to target that
 # site. Without it, the drush command will fail. Use the uri.php file provided by the acsf module to
 # locate the URI based on the site, environment and db role arguments.
 uri=`/usr/bin/env php /mnt/www/html/$site.$env/hooks/acquia/uri.php $site $env $db_role`
 
-# Print a statement to the cloud log.
-echo "$site.$target_env: Running BLT deploy tasks on $uri domain in $env environment on the $site subscription."
-
+# Create array with site name fragments from ACSF uri. 
 IFS='.' read -a name <<< "${uri}"
 
-# Set Drush cache to local ephemeral storage to avoid race conditions. This is
-# done on a per site basis to completely avoid race conditions.
-# @see https://github.com/acquia/blt/pull/2922
-export DRUSH_PATHS_CACHE_DIRECTORY=/tmp/.drush/${db_role}
+# Create and set Drush cache to unique local temporary storage per site.
+# This approach isolates drush processes to completely avoid race conditions
+# that persist after initial attempts at addressing in BLT: https://github.com/acquia/blt/pull/2922
 
-$blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$domain --verbose --yes
+cacheDir=`/usr/bin/env php /mnt/www/html/$site.$env/vendor/acquia/blt/scripts/blt/drush/cache.php $site $env $uri`
+
+# Print to cloud task log.
+echo "Generated temporary drush cache directory: $cacheDir."
+
+# Print to cloud task log.
+echo "Running BLT deploy tasks on $uri domain in $env environment on the $site subscription."
+
+DRUSH_PATHS_CACHE_DIRECTORY=$cacheDir $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$domain --verbose --yes --no-interaction
+
