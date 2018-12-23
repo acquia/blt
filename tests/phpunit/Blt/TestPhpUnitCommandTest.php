625d20f1483382316114793312b816ce5fe82702
Grant Gaudet <grant@greylabel.net>
2018-12-22 17:03:20 -0800

    Fix PHPUnit tests.

--- tests/phpunit/Blt/TestPhpUnitCommandTest.php
+++ tests/phpunit/Blt/TestPhpUnitCommandTest.php
@@ -10,22 +10,6 @@ use Acquia\Blt\Tests\BltProjectTestBase;
 class TestPhpUnitCommandTest extends BltProjectTestBase {
 
   /**
-   * Tests that removing ExampleTest.php doesn't cause failure for users.
-   */
-  public function testPhpUnitCommandNoTests() {
-    $this->fs->remove($this->sandboxInstance . "/tests/phpunit/ExampleTest.php");
-    $repoRroot = $this->config->get("repo.root");
-    list($status_code, $output, $config) = $this->blt("tests:phpunit:run", [
-      "--define" => [
-        "tests.phpunit.0.config=$repoRroot/tests/phpunit/phpunit.xml",
-        "tests.phpunit.0.path=$repoRroot/tests/phpunit",
-        "tests.phpunit.0.class=NULL",
-      ],
-    ]);
-    $this->assertEquals(0, $status_code);
-  }
-
-  /**
    * Test that ExampleTest.php is correctly executed and passes.
    */
   public function testPhpUnitCommandExampleTests() {
@@ -41,6 +25,23 @@ class TestPhpUnitCommandTest extends BltProjectTestBase {
     $results = file_get_contents($this->sandboxInstance . '/reports/phpunit/results.xml');
     $this->assertContains('tests="1" assertions="1"', $results);
     $this->assertContains('name="testExample" class="My\Example\Project\Tests\ExampleTest"', $results);
+  }
+
+  /**
+   * Tests that removing ExampleTest.php doesn't cause failure for users.
+   */
+  public function testPhpUnitCommandNoTests() {
+    $this->fs->remove($this->sandboxInstance . "/tests/phpunit/ExampleTest.php");
+    $docroot = $this->config->get("docroot");
+    $repoRroot = $this->config->get("repo.root");
+    list($status_code, $output, $config) = $this->blt("tests:phpunit:run", [
+      "--define" => [
+        "tests.phpunit.0.config=$docroot/core/phpunit.xml.dist",
+        "tests.phpunit.0.path=$repoRroot/tests/phpunit",
+        "tests.phpunit.0.directory=$repoRroot/tests/phpunit",
+      ],
+    ]);
+    $this->assertEquals(0, $status_code);
   }
 
 }
