d3ac185ea1c974c67980ad7c77119fd2bba697fc
Roderik Muit <rm@wyz.biz>
2019-02-28 21:35:31 +0100

    Replace  by (stable) global variable equivalent (#3408)

--- scripts/factory-hooks/post-sites-php/includes.php
+++ scripts/factory-hooks/post-sites-php/includes.php
@@ -5,8 +5,6 @@
  * ACSF post-sites-php hook.
  *
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
- *
- * phpcs:disable DrupalPractice.CodeAnalysis.VariableAnalysis
  */
 
 // The function_exists check is required as the file is included several times.
@@ -54,7 +52,7 @@ if (!function_exists('gardens_data_get_sites_from_file')) {
 
 }
 // Get all the domains that are defined for the current site.
-$domains = gardens_data_get_sites_from_file($data['gardens_site_settings']['conf']['acsf_db_name']);
+$domains = gardens_data_get_sites_from_file($GLOBALS['gardens_site_settings']['conf']['acsf_db_name']);
 // Get the site's name from the first domain.
 global $_acsf_site_name;
 $_acsf_site_name = explode('.', array_keys($domains)[0])[0];
