176f28f3395be273d06f38ce531b1fa16a1da9bd
David Pagini <dpagini@users.noreply.github.com>
2019-03-08 18:22:48 -0500

    Updating CI settings with a default database. (#3421)

--- settings/ci.settings.php
+++ settings/ci.settings.php
@@ -13,3 +13,23 @@ $settings['file_private_path'] = $dir . '/files-private';
 $settings['trusted_host_patterns'] = array(
   '^.+$',
 );
+
+/**
+ * Sensible CI defaults for databases.
+ *
+ * This will be overridden by system specific CI files.
+ */
+$databases = [
+  'default' => [
+    'default' => [
+      'database' => 'drupal',
+      'username' => 'drupal',
+      'password' => 'drupal',
+      'host' => '127.0.0.1',
+      'port' => '3306',
+      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ],
+  ],
+];
