3056b22a93d776db234654662424605a5dd24fb5
Lindsey Catlett <lindsey.catlett@gmail.com>
2019-03-14 14:22:13 -0400

    Replace  by (stable) global variable equivalent (#3408) (#3457)

--- scripts/factory-hooks/post-sites-php/includes.php
+++ scripts/factory-hooks/post-sites-php/includes.php
@@ -52,7 +52,7 @@ if (!function_exists('gardens_data_get_sites_from_file')) {
 
 }
 // Get all the domains that are defined for the current site.
-$domains = gardens_data_get_sites_from_file($data['gardens_site_settings']['conf']['acsf_db_name']);
+$domains = gardens_data_get_sites_from_file($GLOBALS['gardens_site_settings']['conf']['acsf_db_name']);
 // Get the site's name from the first domain.
 global $_acsf_site_name;
 $_acsf_site_name = explode('.', array_keys($domains)[0])[0];
