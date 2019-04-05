fb110c598f71909523dc6198947fed0a40c2d949
Dane Powell <git@danepowell.com>
2019-04-05 13:52:40 -0700

    Separation of concerns.

--- tests/phpunit/Robo/Commands/CommandTestCase.php
+++ tests/phpunit/Robo/Commands/CommandTestCase.php
@@ -174,6 +174,8 @@ abstract class CommandTestCase extends TestCase {
    */
   protected function createDefaultConfig() {
     $this->config = new DefaultConfig();
+    $this->config->extend(new YamlConfig($this->config->get('blt.root') . '/phing/build.yml',
+      $this->config->toArray()));
     $this->config->extend(new YamlConfig($this->config->get('blt.root') . '/template/blt/blt.yml',
       $this->config->toArray()));
     $this->config->extend(new YamlConfig($this->config->get('blt.root') . '/template/blt/local.yml',
