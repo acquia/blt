5492d81a14933990b687ec4f7ef570d9e1dd0143
Dane Powell <danepowell@users.noreply.github.com>
2019-04-11 13:33:42 -0700

    Fixed coding standards in BLT settings. (#3534)

--- src/Robo/Commands/Setup/SettingsCommand.php
+++ src/Robo/Commands/Setup/SettingsCommand.php
@@ -158,7 +158,7 @@ WARNING;
 
       $result = $this->taskWriteToFile($project_settings_file)
         ->appendUnlessMatches('#vendor/acquia/blt/settings/blt.settings.php#', 'require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";' . "\n")
-        ->appendUnlessMatches('#\# IMPORTANT#', $this->settingsWarning . "\n")
+        ->appendUnlessMatches('#Do not include additional settings here#', $this->settingsWarning . "\n")
         ->append(TRUE)
         ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
         ->run();
