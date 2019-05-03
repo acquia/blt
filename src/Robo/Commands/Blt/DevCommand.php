e7772f7945c881f2bd190432afd30d7045852784
Dane Powell <git@danepowell.com>
2019-05-03 13:16:24 -0700

    Added command to symlink to BLT from projects for development.

---
+++ src/Robo/Commands/Blt/DevCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Acquia\Blt\Robo\Commands\Blt;
+
+use Acquia\Blt\Robo\BltTasks;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Defines commands for developing BLT.
+ */
+class DevCommand extends BltTasks {
+
+  /**
+   * Links to a local BLT package via a Composer path repository.
+   *
+   * @command blt:dev:link-composer
+   * @param array $options
+   */
+  public function linkComposer($options = ['blt-path' => InputOption::VALUE_REQUIRED]) {
+    $composer_json_filepath = $this->getConfigValue('repo.root') . '/composer.json';
+    $composer_json = json_decode(file_get_contents($composer_json_filepath));
+    $composer_json->repositories->blt = [
+      'type' => 'path',
+      'url' => $options['blt-path'],
+    ];
+    $composer_json->require->{'acquia/blt'} = '*';
+
+    file_put_contents($composer_json_filepath, json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+    $this->taskExec('composer update acquia/blt --with-dependencies')
+      ->dir($this->getConfigValue('repo.root'))
+      ->run();
+  }
+
+  // @todo add a command to mount BLT in DrupalVM (see RoboFile::createFromSymlink for example).
+  // Problem is that the symlink placed above has to _also_ be valid within VM.
+}
