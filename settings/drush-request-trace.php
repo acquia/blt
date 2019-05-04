4986c52deedf00e20ae2ea178ce4f6563cbfe0b4
Rohit Joshi <joshi.rohit100@gmail.com>
2019-05-04 14:31:01 +0530

    Tracing the Drupal requests in drupal-requests.log on acquia cloud.

---
+++ settings/drush-request-trace.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Traces drush requests in drupal-requests.log logs.
+ */
+
+/**
+ * Adds logging information for drush requests in drupal-requests.log on Acquia.
+ * By default these requests show up with no REQUEST_METHOD or URI, which can
+ * make splitting them up very hard.
+ */
+
+if (isset($_ENV['AH_SITE_ENVIRONMENT']) && PHP_SAPI === 'cli') {
+  // Set the `request method`.
+  putenv('REQUEST_METHOD=CLI');
+  // Set the `domain`.
+  putenv('HTTP_HOST=' . $_SERVER['HTTP_HOST']);
+
+  if (function_exists('drush_get_context')) {
+    $cli_args = drush_get_context('argv');
+    $cli_args[0] = 'drush';
+
+    // Ensure each argument is wrapped in quotes.
+    $cli_args = array_map(function ($value) {
+      $escaped = escapeshellarg($value);
+      return ("'$value'" === $escaped) ? $value : $escaped;
+    }, $cli_args);
+
+    // Prepare the uri.
+    $uri = implode(' ', $cli_args);
+
+    // Set the `request uri`.
+    putenv('REQUEST_URI=' . $uri);
+  }
+}
