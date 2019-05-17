64d8d72e60db08ca66cc1d19bd10410e3fac83a3
Dane Powell <git@danepowell.com>
2019-05-17 16:04:24 -0700

    Avoid writing to config.yml multiple times.

--- src/Robo/Commands/Blt/DevCommand.php
+++ src/Robo/Commands/Blt/DevCommand.php
@@ -10,6 +10,12 @@ use Symfony\Component\Yaml\Yaml;
  */
 class DevCommand extends BltTasks {
 
+  // Map host BLT path to guest BLT path so that symlinks work in guest.
+  const DIRECTORY_MAPPING = [
+    '../../packages/blt' => '/var/packages/blt',
+    '../blt' => '/var/www/blt',
+  ];
+
   /**
    * Links to a local BLT package via a Composer path repository.
    *
@@ -21,31 +27,26 @@ class DevCommand extends BltTasks {
    * @param array $options
    */
   public function linkComposer($options = ['blt-path' => '../../packages/blt']) {
-    $composer_json_filepath = $this->getConfigValue('repo.root') . '/composer.json';
-    $composer_json = json_decode(file_get_contents($composer_json_filepath));
-    $composer_json->repositories->blt = [
-      'type' => 'path',
-      'url' => $options['blt-path'],
-    ];
-    $composer_json->require->{'acquia/blt'} = '*';
-
-    file_put_contents($composer_json_filepath, json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    $this->taskExec("composer config repositories.blt path {$options['blt-path']} && composer require acquia/blt:* --no-update")
+      ->dir($this->getConfigValue('repo.root'))
+      ->run();
 
     $this->taskExec('rm -rf vendor && composer update acquia/blt --with-dependencies')
       ->dir($this->getConfigValue('repo.root'))
       ->run();
-
     $projectDrupalVmConfigFile = $this->getConfigValue('vm.config');
-    // Map host BLT path to guest BLT path so that symlinks work in guest.
-    $directoryMapping = [
-      '../../packages/blt' => '/var/packages/blt',
-      '../blt' => '/var/www/blt',
-    ];
-    if ($projectDrupalVmConfigFile && isset($directoryMapping[$options['blt-path']])) {
+
+    if ($projectDrupalVmConfigFile && isset(self::DIRECTORY_MAPPING[$options['blt-path']])) {
       $vm_config = Yaml::parse(file_get_contents($projectDrupalVmConfigFile));
+      $existing_entry = array_filter($vm_config['vagrant_synced_folders'], function ($folder) {
+        return in_array($folder['destination'], self::DIRECTORY_MAPPING);
+      });
+      if ($existing_entry) {
+        return;
+      }
       $vm_config['vagrant_synced_folders'][] = [
         'local_path' => $options['blt-path'],
-        'destination' => $directoryMapping[$options['blt-path']],
+        'destination' => self::DIRECTORY_MAPPING[$options['blt-path']],
         'type' => 'nfs',
       ];
       file_put_contents($projectDrupalVmConfigFile, Yaml::dump($vm_config, 4));
