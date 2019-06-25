72fe6c4286832d6fba6161867895b55c42e172f9
Lindsey Catlett <lindsey.catlett@gmail.com>
2019-06-25 12:18:06 -0400

    Update memcache settings to new Acquia recommendations supported by stable release of module.

--- settings/memcache.settings.php
+++ settings/memcache.settings.php
@@ -17,15 +17,25 @@ if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
   if ($memcached_exists) {
     $settings['memcache']['extension'] = 'Memcached';
   }
-
   if (class_exists(ClassLoader::class)) {
     $class_loader = new ClassLoader();
-    $class_loader->addPsr4('Drupal\\memcache\\', DRUPAL_ROOT . '/modules/contrib/memcache/src');
+    $class_loader->addPsr4('Drupal\\memcache\\', 'modules/contrib/memcache/src');
     $class_loader->register();
     $settings['container_yamls'][] = $memcache_services_yml;
 
-    // Define custom bootstrap container definition to use Memcache for
-    // cache.container.
+    // Acquia Default Settings for the memcache module
+    // Default settings for the Memcache module
+
+     // Enable compression for PHP 7.
+    $settings['memcache']['options'][Memcached::OPT_COMPRESSION] = TRUE;
+
+     // Ensure key_prefix is set to avoid drush cr flushing all memcache bins on multisite
+    $settings['memcache']['key_prefix'] = $conf['acquia_hosting_site_info']['db']['name'] . '_';
+
+    // Settings for SASL Authenticated Memcached.
+    $settings['memcache']['options'][Memcached::OPT_BINARY_PROTOCOL] = TRUE;
+
+    // Bootstrap cache.container with memcache rather than database.
     $settings['bootstrap_container_definition'] = [
       'parameters' => [],
       'services' => [
@@ -51,6 +61,10 @@ if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
           'factory' => ['@memcache.backend.cache.factory', 'get'],
           'arguments' => ['container'],
         ],
+        'lock.container' => [
+          'class' => 'Drupal\memcache\Lock\MemcacheLockBackend',
+          'arguments' => ['container', '@memcache.backend.cache.container'],
+        ],
         'cache_tags_provider.container' => [
           'class' => 'Drupal\Core\Cache\DatabaseCacheTagsChecksum',
           'arguments' => ['@database'],
@@ -61,19 +75,20 @@ if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
             'container',
             '@memcache.backend.cache.container',
             '@cache_tags_provider.container',
+            '@lock.container',
           ],
         ],
       ],
     ];
 
-    // Override default fastchained backend for static bins.
-    // @see https://www.drupal.org/node/2754947
+    // Use memcache for bootstrap, discovery, config instead of fast chained backend to properly
+    // invalidate caches on multiple webs. See https://www.drupal.org/node/2754947
+
     $settings['cache']['bins']['bootstrap'] = 'cache.backend.memcache';
     $settings['cache']['bins']['discovery'] = 'cache.backend.memcache';
     $settings['cache']['bins']['config'] = 'cache.backend.memcache';
 
     // Use memcache as the default bin.
     $settings['cache']['default'] = 'cache.backend.memcache';
-
   }
-}
+ }
