3a11be5be3cd47ca5c90f5986bc61e8f20bda365
Dane Powell <git@danepowell.com>
2019-07-09 14:57:37 -0700

    Removed unused context.

--- subtree-splits/blt-project/tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ subtree-splits/blt-project/tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -3,7 +3,6 @@
 namespace Drupal;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Behat\Behat\Context\SnippetAcceptingContext;
 
 /**
  * FeatureContext class defines custom step definitions for Behat.
