8f97ed19db8e9e4167f6e50de6a2d09e89d41933
Dane Powell <git@danepowell.com>
2019-09-16 14:23:16 -0700

    Fixes #3849: Drush alias check fails on Windows

--- src/Robo/Inspector/Inspector.php
+++ src/Robo/Inspector/Inspector.php
@@ -283,7 +283,7 @@ class Inspector implements BuilderAwareInterface, ConfigAwareInterface, Containe
    */
   public function isDrushAliasValid($alias) {
     $bin = $this->getConfigValue('composer.bin');
-    $command = "'$bin/drush' site:alias @$alias --format=json";
+    $command = "\"$bin/drush\" site:alias @$alias --format=json";
     return $this->executor->execute($command)
       ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERY_VERBOSE)
       ->run()
