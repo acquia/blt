b3763d9e095038bbb8569c1cd713b3708d12afe5
Brian Osborne <bosborne@princeton.edu>
2019-10-01 09:28:45 -0400

    Issue #3865: Move location of logging UI override to appropriate file.

--- settings/cache.settings.php
+++ settings/cache.settings.php
@@ -8,16 +8,6 @@
 use Acquia\Blt\Robo\Common\EnvironmentDetector;
 
 /**
- * Settings.
- *
- * @var array $settings
- */
-
-if (EnvironmentDetector::isProdEnv() || EnvironmentDetector::isStageEnv()) {
-  $config['system.logging']['error_level'] = 'hide';
-}
-
-/**
  * Use memcache as cache backend if Acquia configuration is present.
  */
 $repo_root = dirname(DRUPAL_ROOT);
