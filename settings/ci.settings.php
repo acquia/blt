8954b071da92f7b1a6b75b26ac553ee3309edf5f
Dane Powell <git@danepowell.com>
2020-01-24 13:07:57 -0800

    DT-1108: Fixes #3965: Stop setting tmp file path. (#3991)

--- settings/ci.settings.php
+++ settings/ci.settings.php
@@ -6,7 +6,6 @@
  */
 
 $config['system.logging']['error_level'] = 'verbose';
-$config['system.file']['path']['temporary'] = '/tmp';
 
 $dir = dirname(DRUPAL_ROOT);
 $settings['file_private_path'] = $dir . '/files-private';
