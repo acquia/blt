db269dccc7114ffbc88a6ff45ca967cb70809e1c
Dane Powell <git@danepowell.com>
2020-02-25 21:37:21 -0800

    DT-1179: Moved probo integration to plugin.

--- src/Robo/Commands/Recipes/CiCommand.php
+++ src/Robo/Commands/Recipes/CiCommand.php
@@ -56,27 +56,6 @@ class CiCommand extends BltTasks {
   }
 
   /**
-   * Initializes default Probo CI configuration for this project.
-   *
-   * @command recipes:ci:probo:init
-   *
-   * @aliases rcpri ci:probo:init
-   */
-  public function proboInit() {
-    $result = $this->taskFilesystemStack()
-      ->copy($this->getConfigValue('blt.root') . '/scripts/probo/.probo.yml', $this->getConfigValue('repo.root') . '/.probo.yml', TRUE)
-      ->stopOnFail()
-      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-      ->run();
-
-    if (!$result->wasSuccessful()) {
-      throw new BltException("Could not initialize Probo CI configuration.");
-    }
-
-    $this->say("<info>A pre-configured .probo.yml file was copied to your repository root.</info>");
-  }
-
-  /**
    * Initializes default GitLab Pipelines configuration for this project.
    *
    * @command recipes:ci:gitlab:init
