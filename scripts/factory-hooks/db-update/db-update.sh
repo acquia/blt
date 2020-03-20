2caa888f1ffa5abd12e01716de4cc547cfd19bd8
Lindsey Catlett <lindsey.catlett@gmail.com>
2020-03-20 13:35:31 -0400

    Merge improvements implemented in post-site-install.

--- scripts/factory-hooks/db-update/db-update.sh
+++ scripts/factory-hooks/db-update/db-update.sh
@@ -7,47 +7,47 @@
 # regular database update (drush updatedb) command. So that update command will
 # normally be part of the commands executed below.
 #
-# Usage: post-code-deploy site env db-role domain custom-arg
+# Usage: '/mnt/www/html/site.env/factory-hooks/db-update/db-update.sh' 'site' 'env' 'db-role' 'domain'
 # Map the script inputs to convenient names.
 
 # Exit immediately on error and enable verbose log output.
 set -ev
 
-# Acquia hosting site / environment names
-site="$1"
+# Map the script inputs to convenient names:
+# Acquia Hosting sitegroup (application) and environment.
+sitegroup="$1"
 env="$2"
-# database role. (Not expected to be needed in most hook scripts.)
+# Database role. This is a truly unique identifier for an ACSF site and is e.g.
+# part of the public files path.
 db_role="$3"
-# The public domain name of the website. If the site uses a path based domain,
-# the path is appended (without trailing slash), e.g. "domain.com/subpath".
-domain="$4"
+# Internal (Acquia managed) domain name of the website. (No public domain name
+# is assigned yet, immediately after installation.) The first part is a name
+# that is unique per installed site. A small but significant difference with
+# $db_role: if a site gets deleted and reinstalled with the same name, it gets
+# a different $db_role.
+internal_domain="$4"
+# To get only the site name in ${name[0]}:
+IFS='.' read -a name <<< $internal_domain
 
 # BLT executable:
-blt="/mnt/www/html/$site.$env/vendor/acquia/blt/bin/blt"
-
-# You need the URI of the site factory website in order for drush to target that
-# site. Without it, the drush command will fail. Use the uri.php file provided by the acsf module to
-# locate the URI based on the site, environment and db role arguments.
-uri=`/usr/bin/env php /mnt/www/html/$site.$env/hooks/acquia/uri.php $site $env $db_role`
-
-# Create array with site name fragments from ACSF uri.
-IFS='.' read -a name <<< "${uri}"
+blt="/mnt/www/html/$sitegroup.$env/vendor/acquia/blt/bin/blt"
 
 # Create and set Drush cache to unique local temporary storage per site.
 # This approach isolates drush processes to completely avoid race conditions
 # that persist after initial attempts at addressing in BLT: https://github.com/acquia/blt/pull/2922
+cache_dir=`/usr/bin/env php /mnt/www/html/$sitegroup.$env/vendor/acquia/blt/scripts/blt/drush/cache.php $sitegroup $env $internal_domain`
 
-cacheDir=`/usr/bin/env php /mnt/www/html/$site.$env/vendor/acquia/blt/scripts/blt/drush/cache.php $site $env $uri`
+# Execute the updates.
+DRUSH_PATHS_CACHE_DIRECTORY="$cache_dir" $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$internal_domain --verbose --no-interaction
+result=$?
 
-# Print to cloud task log.
-echo "Generated temporary drush cache directory: $cacheDir."
-
-# Print to cloud task log.
-echo "Running BLT deploy tasks on $uri domain in $env environment on the $site subscription."
-
-# Update Drupal. The trailing slash behind the domain works around a bug in
-# Drush < 9.6 for path based domains: "domain.com/subpath/" is considered a
-# valid URI but "domain.com/subpath" is not.
-DRUSH_PATHS_CACHE_DIRECTORY=$cacheDir $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$domain/ --verbose --no-interaction
+# Clean up the drush cache directory.
+rm -rf "$cache_dir"
 
 set +v
+
+# Exit with the status of the BLT commmand. If the exit status is non-zero,
+# Site Factory will send a notification of a partiolly failed install and will
+# stop executing any further post-site-install hook scripts that would be in
+# this directory (and get executed in alphabetical order).
+exit $result
