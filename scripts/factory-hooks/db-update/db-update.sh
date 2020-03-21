0af2831946c2a885341e424ac5c07df4c3473ce6
Lindsey Catlett <lindsey.catlett@gmail.com>
2020-03-20 23:29:02 -0400

    Restore previous public domain logic, update usage comments, remove exit code behavior.

--- scripts/factory-hooks/db-update/db-update.sh
+++ scripts/factory-hooks/db-update/db-update.sh
@@ -7,7 +7,7 @@
 # regular database update (drush updatedb) command. So that update command will
 # normally be part of the commands executed below.
 #
-# Usage: '/mnt/www/html/site.env/factory-hooks/db-update/db-update.sh' 'site' 'env' 'db-role' 'domain'
+# Usage: db-update.sh sitegroup env db-role domain custom-arg
 # Map the script inputs to convenient names.
 
 # Exit immediately on error and enable verbose log output.
@@ -20,14 +20,20 @@ env="$2"
 # Database role. This is a truly unique identifier for an ACSF site and is e.g.
 # part of the public files path.
 db_role="$3"
-# Internal (Acquia managed) domain name of the website. (No public domain name
-# is assigned yet, immediately after installation.) The first part is a name
-# that is unique per installed site. A small but significant difference with
-# $db_role: if a site gets deleted and reinstalled with the same name, it gets
-# a different $db_role.
-internal_domain="$4"
-# To get only the site name in ${name[0]}:
-IFS='.' read -a name <<< $internal_domain
+# The public domain name of the website. If the site uses a path based domain,
+# the path is appended (without trailing slash), e.g. "domain.com/subpath".
+domain="$4"
+
+# BLT executable:
+blt="/mnt/www/html/$sitegroup.$env/vendor/acquia/blt/bin/blt"
+
+# You need the URI of the site factory website in order for drush to target that
+# site. Without it, the drush command will fail. Use the uri.php file provided by the acsf module to
+# locate the URI based on the site, environment and db role arguments.
+uri=`/usr/bin/env php /mnt/www/html/$sitegroup.$env/hooks/acquia/uri.php $sitegroup $env $db_role`
+
+# Create array with site name fragments from ACSF uri.
+IFS='.' read -a name <<< "${uri}"
 
 # BLT executable:
 blt="/mnt/www/html/$sitegroup.$env/vendor/acquia/blt/bin/blt"
@@ -35,19 +41,23 @@ blt="/mnt/www/html/$sitegroup.$env/vendor/acquia/blt/bin/blt"
 # Create and set Drush cache to unique local temporary storage per site.
 # This approach isolates drush processes to completely avoid race conditions
 # that persist after initial attempts at addressing in BLT: https://github.com/acquia/blt/pull/2922
-cache_dir=`/usr/bin/env php /mnt/www/html/$sitegroup.$env/vendor/acquia/blt/scripts/blt/drush/cache.php $sitegroup $env $internal_domain`
+cache_dir=`/usr/bin/env php /mnt/www/html/$sitegroup.$env/vendor/acquia/blt/scripts/blt/drush/cache.php $sitegroup $env $uri`
 
-# Execute the updates.
-DRUSH_PATHS_CACHE_DIRECTORY="$cache_dir" $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$internal_domain --verbose --no-interaction
-result=$?
+echo "Generated temporary drush cache directory: $cache_dir."
+
+echo "Running BLT deploy tasks on $uri domain in $env environment on the $sitegroup subscription."
+
+# Run blt drupal:update tasks The trailing slash behind the domain works around a bug in
+# Drush < 9.6 for path based domains: "domain.com/subpath/" is considered a
+# valid URI but "domain.com/subpath" is not.
+DRUSH_PATHS_CACHE_DIRECTORY="$cache_dir" $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$domain/ --verbose --no-interaction
 
 # Clean up the drush cache directory.
+echo "Removing temporary drush cache files."
 rm -rf "$cache_dir"
 
 set +v
 
-# Exit with the status of the BLT commmand. If the exit status is non-zero,
-# Site Factory will send a notification of a partiolly failed install and will
-# stop executing any further post-site-install hook scripts that would be in
-# this directory (and get executed in alphabetical order).
-exit $result
+# @todo Exit with the status of the BLT commmand. If the exit status is non-zero,
+# Site Factory will send a notification of a failed 'blt drupal:update',
+# interruptting the execution of additional db-update scripts.
