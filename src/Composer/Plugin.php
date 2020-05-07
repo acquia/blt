b033b0499c047f00daac1e5522299f5b35cc9d7a
Dane Powell <git@danepowell.com>
2020-05-07 09:36:52 -0700

    Fixed code standards.

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -69,9 +69,15 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
     $this->executor = new ProcessExecutor($this->io);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function deactivate(Composer $composer, IOInterface $io) {
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function uninstall(Composer $composer, IOInterface $io) {
   }
 
