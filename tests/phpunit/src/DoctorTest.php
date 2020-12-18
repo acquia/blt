d3cd4c2e09a5e5f281a54c1bdd47ac2e257bc6e6
Dane Powell <git@danepowell.com>
2020-12-17 17:26:27 -0800

    Fix test

--- tests/phpunit/src/DoctorTest.php
+++ tests/phpunit/src/DoctorTest.php
@@ -9,7 +9,6 @@ class DoctorTest extends BltProjectTestBase {
 
   public function testDoctorCommand() {
     $this->blt('blt:init:settings');
-    $this->blt('setup:behat');
     list($status_code) = $this->blt("doctor");
     $this->assertEquals(0, $status_code);
   }
