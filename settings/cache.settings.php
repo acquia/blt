84fc9f6455f3e86e265760092f798c6f86391294
Vincent <vincent.bouchet@acquia.com>
2021-01-25 08:46:12 +0100

    Update cache.settings.php for coding standard and require_once
    File was using mixed variable naming convention.
    File was included using require but the file may be included multiple times.

--- settings/cache.settings.php
+++ settings/cache.settings.php
@@ -9,8 +9,8 @@
  * Use memcache as cache backend if Acquia configuration is present.
  */
 $repo_root = dirname(DRUPAL_ROOT);
-$memcacheSettingsFile = $repo_root . '/vendor/acquia/memcache-settings/memcache.settings.php';
-if (file_exists($memcacheSettingsFile)) {
+$memcache_settings_file = $repo_root . '/vendor/acquia/memcache-settings/memcache.settings.php';
+if (file_exists($memcache_settings_file)) {
   // phpcs:ignore
-  require $memcacheSettingsFile;
+  require_once $memcache_settings_file;
 }
