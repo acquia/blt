e00bc280852ab375080f62adda983c8bc079b69a
Dane Powell <git@danepowell.com>
2021-01-27 14:49:50 -0800

    DX-3168: invokeCommand() doesn't preserve input options (#4324)
    * DX-3168: invokeCommand() doesn't preserve input options
    
    * code style cleanup
    
    * Hardcode environment option

--- src/Robo/BltTasks.php
+++ src/Robo/BltTasks.php
@@ -84,8 +84,16 @@ class BltTasks implements ConfigAwareInterface, InspectorAwareInterface, LoggerA
       $application = $this->getContainer()->get('application');
       $command = $application->find($command_name);
 
-      $input = new ArrayInput($args);
+      // Build a new input object that inherits options from parent command.
+      $definition = $application->getDefinition();
+      $args['command'] = $command_name;
+      if ($this->input()->hasOption('environment')) {
+        $args['--environment'] = $this->input()->getOption('environment');
+      }
+      $input = new ArrayInput($args, $definition);
       $input->setInteractive($this->input()->isInteractive());
+
+      // Now run the command.
       $prefix = str_repeat(">", $this->invokeDepth);
       $this->output->writeln("<comment>$prefix $command_name</comment>");
       $exit_code = $application->runCommand($command, $input, $this->output());
