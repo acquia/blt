bc1ea73061a9969e6ceeec96214af290f1d2a8a6
Nilesh Lohar <nilesh.lohar@acquia.com>
2021-01-29 23:32:26 +0530

    Adding --no-sandbox option to launchChrome. (#4328)

--- src/Robo/Commands/Tests/TestsCommandBase.php
+++ src/Robo/Commands/Tests/TestsCommandBase.php
@@ -104,7 +104,7 @@ class TestsCommandBase extends BltTasks {
     $this->logger->info("Launching headless chrome...");
     $this->getContainer()
       ->get('executor')
-      ->execute("'$chrome_bin' --headless --disable-web-security --remote-debugging-port={$this->chromePort} {$this->chromeArgs} $chrome_host")
+      ->execute("'$chrome_bin' --headless --no-sandbox --disable-web-security --remote-debugging-port={$this->chromePort} {$this->chromeArgs} $chrome_host")
       ->background(TRUE)
       ->printOutput(TRUE)
       ->printMetadata(TRUE)
