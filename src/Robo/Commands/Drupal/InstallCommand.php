c121b8eeba29a67942ee8d1a12508561071f56cc
Pavlos Daniel <pavlos.daniel@acquia.com>
2021-11-23 02:33:36 +0000

    Define an account password during install if one is given.

--- src/Robo/Commands/Drupal/InstallCommand.php
+++ src/Robo/Commands/Drupal/InstallCommand.php
@@ -120,6 +120,11 @@ class InstallCommand extends BltTasks {
       ->verbose(TRUE)
       ->printOutput(TRUE);
 
+    // Allow installs to define a custom user 1 password.
+    if ($this->getConfigValue('drupal.account.pass') !== NULL) {
+      $task->option('account-pass', $this->getConfigValue('drupal.account.pass'));
+    }
+
     // Install site from existing config if supported.
     $strategy = $this->getConfigValue('cm.strategy');
     $cm_core_key = 'sync';
