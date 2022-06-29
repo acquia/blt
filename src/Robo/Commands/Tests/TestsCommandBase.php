4eb0b8b3787a1b6ecbf1d040ed40cc6c89c3b620
Mike Madison <mike.madison@acquia.com>
2022-06-29 08:33:19 -0700

    removing single quotes.

--- src/Robo/Commands/Tests/TestsCommandBase.php
+++ src/Robo/Commands/Tests/TestsCommandBase.php
@@ -104,7 +104,7 @@ class TestsCommandBase extends BltTasks {
     $this->logger->info("Launching headless chrome...");
     $this->getContainer()
       ->get('executor')
-      ->executeShell("'$chrome_bin' --headless --no-sandbox --disable-web-security --remote-debugging-port={$this->chromePort} {$this->chromeArgs} $chrome_host")
+      ->executeShell("$chrome_bin --headless --no-sandbox --disable-web-security --remote-debugging-port={$this->chromePort} {$this->chromeArgs} $chrome_host")
       ->background(TRUE)
       ->printOutput(TRUE)
       ->printMetadata(TRUE)
