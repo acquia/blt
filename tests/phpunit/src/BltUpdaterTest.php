3fbec978e18313fa7635887c72af4df4122d15a3
Mike Madison <mike.madison@acquia.com>
2022-10-27 14:47:36 -0700

    Fixes #4618: adding failing test.

---
+++ tests/phpunit/src/BltUpdaterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Acquia\Blt\Tests;
+
+/**
+ * Test Updater commands.
+ */
+class BltUpdaterTest extends BltProjectTestBase {
+  /**
+   * Tests internal:add-to-project command.
+   */
+  public function testConfigSplitRecipe() {
+    [$status_code] = $this->blt('internal:add-to-project');
+    $this::assertEquals(0, $status_code);
+  }
+}
