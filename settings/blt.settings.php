1bb209fbd7719f66979b56de076d90abca87908f
Brian Tully <brian.tully@acquia.com>
2022-11-28 10:51:33 -0500

    Fixes #4632 comment to indicate where Acquia include file is set (#4633)
    * Fixes #4632 comment to indicate where Acquia include file is set
    
    * fix tests
    
    * ahem... fix tests
    
    Co-authored-by: Dane Powell <git@danepowell.com>

--- settings/blt.settings.php
+++ settings/blt.settings.php
@@ -86,6 +86,8 @@ $site_name = EnvironmentDetector::getSiteName($site_path);
 if (EnvironmentDetector::isAhEnv()) {
   try {
     if (!EnvironmentDetector::isAcsfEnv()) {
+      // Pull in Acquia environment settings file. This eliminates the need to
+      // add the Acquia require line in settings.php.
       $settings_files[] = FilePaths::ahSettingsFile(EnvironmentDetector::getAhGroup(), $site_name);
     }
   }
