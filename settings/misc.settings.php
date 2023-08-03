2d1fe190da6b1cbf199005683d35a7c9b0dd8cdf
Alejandro Garza <janogarza@gmail.com>
2023-08-03 13:36:18 -0400

    Allow early setting of hash_salt

--- settings/misc.settings.php
+++ settings/misc.settings.php
@@ -22,7 +22,9 @@
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = file_get_contents(DRUPAL_ROOT . '/../salt.txt');
+if (empty($settings['hash_salt']) {
+  $settings['hash_salt'] = file_get_contents(DRUPAL_ROOT . '/../salt.txt');
+}
 
 /**
  * Deployment identifier.
